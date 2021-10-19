392101d6703b31b5ad022eaab245261ef00d846c
ngwoon <todory2002@gmail.com>
2021-10-19 22:57:49 +0900

    feat: 공지 API 테스트코드 작성 및 API 문서 최신화
    API 문서에 요청 파라미터 제약조건 필드, 잘못된 예시를 추가했다.

--- src/test/java/com/kustacks/kuring/controller/NoticeControllerTest.java
+++ src/test/java/com/kustacks/kuring/controller/NoticeControllerTest.java
@@ -1,6 +1,7 @@
 package com.kustacks.kuring.controller;
 
 import com.kustacks.kuring.controller.dto.NoticeDTO;
+import com.kustacks.kuring.error.ErrorCode;
 import com.kustacks.kuring.service.NoticeService;
 import org.junit.Before;
 import org.junit.Rule;
@@ -10,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.restdocs.payload.JsonFieldType;
@@ -33,6 +35,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWit
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -50,7 +53,16 @@ public class NoticeControllerTest {
     private NoticeService noticeService;
 
     @Autowired
-    WebApplicationContext webApplicationContext;
+    private WebApplicationContext webApplicationContext;
+
+    private final String articleId = "5cw2e1";
+    private final String postedDate = "20211016";
+    private final String subject = "[학사] 2021년 학사 관련 공지";
+    private final String categoryName = "bachelor";
+
+    private String type;
+    private int offset;
+    private int max;
 
     @Before
     public void setUp() {
@@ -59,18 +71,13 @@ public class NoticeControllerTest {
                 .build();
     }
 
-    @DisplayName("학사 카테고리의 공지 가져오기")
+    @DisplayName("공지 API - 성공")
     @Test
-    public void getNoticesTest() throws Exception {
+    public void getNoticesSuccessTest() throws Exception {
 
-        String type = "bch";
-        int offset = 0;
-        int max = 10;
-
-        String articleId = "5cw2e1";
-        String postedDate = "20211016";
-        String subject = "[학사] 2021년 학사 관련 공지";
-        String categoryName = "bachelor";
+        type = "bch";
+        offset = 0;
+        max = 10;
 
         List<NoticeDTO> noticeDTOList = new LinkedList<>();
         noticeDTOList.add(NoticeDTO.builder()
@@ -97,13 +104,16 @@ public class NoticeControllerTest {
                 .andExpect(jsonPath("resultCode").value(200))
                 .andExpect(jsonPath("baseUrl").exists())
                 .andExpect(jsonPath("noticeList").exists())
-                .andDo(document("notice",
+                .andDo(document("notice-success",
                                 getDocumentRequest(),
                                 getDocumentResponse(),
                                 requestParameters(
-                                        parameterWithName("type").description("공지 카테고리 키워드"),
-                                        parameterWithName("offset").description("가져올 공지의 시작 인덱스"),
+                                        parameterWithName("type").description("공지 카테고리 키워드")
+                                                .attributes(key("Constraints").value("bch, sch, emp, nat, stu, ind, nor, lib")),
+                                        parameterWithName("offset").description("가져올 공지의 시작 인덱스")
+                                                .attributes(key("Constraints").value("0 이상의 정수")),
                                         parameterWithName("max").description("가져올 공지 최대 개수")
+                                                .attributes(key("Constraints").value("1 이상 30 이하의 정수"))
                                 ),
                                 responseFields(
                                         fieldWithPath("isSuccess").type(JsonFieldType.BOOLEAN).description("성공 여부"),
@@ -116,6 +126,58 @@ public class NoticeControllerTest {
                                         fieldWithPath("noticeList[].category").type(JsonFieldType.STRING).description("공지 카테고리명")
                                 ))
 
+                );
+    }
+
+    @DisplayName("공지 API - 실패 - 잘못된 공지 카테고리")
+    @Test
+    public void getNoticesFailByInvalidTypeTest() throws Exception {
+        type = "invalid-type";
+        offset = 0;
+        max = 20;
+
+        // when
+        ResultActions result = mockMvc.perform(get("/api/v1/notice")
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaType.APPLICATION_JSON)
+                .queryParam("type", type)
+                .queryParam("offset", String.valueOf(offset))
+                .queryParam("max", String.valueOf(max)));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("isSuccess").value(false))
+                .andExpect(jsonPath("resultMsg").value(ErrorCode.API_NOTICE_NOT_EXIST_CATEGORY.getMessage()))
+                .andExpect(jsonPath("resultCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andDo(document("notice-fail-invalid-category",
+                        getDocumentRequest(),
+                        getDocumentResponse())
+                );
+    }
+
+    @DisplayName("공지 API - 실패 - 잘못된 offset 혹은 max 파라미터 값")
+    @Test
+    public void getNoticesFailByInvalidOffsetTest() throws Exception {
+        type = "bch";
+        offset = -1;
+        max = 20;
+
+        // when
+        ResultActions result = mockMvc.perform(get("/api/v1/notice")
+                .characterEncoding(StandardCharsets.UTF_8)
+                .accept(MediaType.APPLICATION_JSON)
+                .queryParam("type", type)
+                .queryParam("offset", String.valueOf(offset))
+                .queryParam("max", String.valueOf(max)));
+
+        // then
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("isSuccess").value(false))
+                .andExpect(jsonPath("resultMsg").value(ErrorCode.API_INVALID_PARAM.getMessage()))
+                .andExpect(jsonPath("resultCode").value(HttpStatus.BAD_REQUEST.value()))
+                .andDo(document("notice-fail-invalid-param",
+                        getDocumentRequest(),
+                        getDocumentResponse())
                 );
     }
 }
