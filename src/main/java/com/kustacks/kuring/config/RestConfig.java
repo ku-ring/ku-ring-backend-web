7bc82e7e55b65dbde107267fb3f3c601cebadf18
ngwoon <todory2002@gmail.com>
2022-01-04 02:31:43 +0900

    [KSS-200] style: 설정파일 분리
    KuringApplication의 설정 관련 애노테이션을 별개 파일로 분리했다.

---
+++ src/main/java/com/kustacks/kuring/config/RestConfig.java
@@ -0,0 +1,25 @@
+package com.kustacks.kuring.config;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+        factory.setConnectTimeout(5000);  //5초
+        factory.setReadTimeout(5000);     //5초
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(5)
+                .build();
+        factory.setHttpClient(httpClient);
+        return new RestTemplate(factory);
+    }
+}
---
+++ src/main/java/com/kustacks/kuring/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.kustacks.kuring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
