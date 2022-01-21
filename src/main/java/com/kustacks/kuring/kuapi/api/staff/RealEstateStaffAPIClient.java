877e502f7d91161a3f1d0fd6810c59b3e2be8905
ngwoon <todory2002@gmail.com>
2022-01-21 18:11:43 +0900

    [KSS-217] fix: 부동산학과 교직원 스크래핑 문제
    부동산학과의 HTML을 받아오는 방법에 오류가 있음을 확인했다. 정확하지는 않으나, 해외 ip 문제가 또 발생한 것으로 보인다. 우선 proxylist 사이트에서 유효한 한국 프록시 네 곳을 선정해서 이곳을 통해 HTML을 받아오도록 수정했다.

--- src/main/java/com/kustacks/kuring/kuapi/api/staff/RealEstateStaffAPIClient.java
+++ src/main/java/com/kustacks/kuring/kuapi/api/staff/RealEstateStaffAPIClient.java
@@ -21,8 +21,8 @@ public class RealEstateStaffAPIClient implements StaffAPIClient {
 
     private final JsoupClient jsoupClient;
 
-    public RealEstateStaffAPIClient(JsoupClient normalJsoupClient) {
-        this.jsoupClient = normalJsoupClient;
+    public RealEstateStaffAPIClient(JsoupClient proxyJsoupClient) {
+        this.jsoupClient = proxyJsoupClient;
     }
 
     @Override
