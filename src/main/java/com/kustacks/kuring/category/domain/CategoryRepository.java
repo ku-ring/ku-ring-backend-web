8b7839665ccf7fd34879baf413f0c11066b5d63a
jiwoo <zbqmgldjfh@gmail.com>
2023-01-25 20:29:08 +0900

    refactor: 모든 JpaRepository에서 @Repository 제거
    Spring Data Jpa가 자동으로 구현체를 만들어 애노테이션을 추가해주기에 추가할 필요가 없다

--- src/main/java/com/kustacks/kuring/category/domain/CategoryRepository.java
+++ src/main/java/com/kustacks/kuring/category/domain/CategoryRepository.java
@@ -1,12 +1,10 @@
 package com.kustacks.kuring.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, String> {
     default Map<String, Category> findAllMap() {
         return findAll().stream().collect(Collectors.toMap(Category::getName, v -> v));
--- src/main/java/com/kustacks/kuring/feedback/domain/FeedbackRepository.java
+++ src/main/java/com/kustacks/kuring/feedback/domain/FeedbackRepository.java
@@ -1,9 +1,6 @@
 package com.kustacks.kuring.feedback.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
 }
