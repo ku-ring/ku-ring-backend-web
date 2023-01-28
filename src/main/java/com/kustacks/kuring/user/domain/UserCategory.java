e6331b024d4b7f86f6eda35f7d3282e29408090c
jiwoo <zbqmgldjfh@gmail.com>
2023-01-28 22:35:59 +0900

    refactor: UserCategory Entity 리팩토링

--- src/main/java/com/kustacks/kuring/user/domain/UserCategory.java
+++ src/main/java/com/kustacks/kuring/user/domain/UserCategory.java
@@ -1,22 +1,22 @@
 package com.kustacks.kuring.user.domain;
 
 import com.kustacks.kuring.category.domain.Category;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-@Table(name = "user_category")
 public class UserCategory {
 
     @Id
@@ -24,11 +24,11 @@ public class UserCategory {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_token", referencedColumnName = "token", nullable = false)
     private User user;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_name", nullable = false)
     private Category category;
 
