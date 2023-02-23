ef475886d9d5f437949208881d482fc47c120e2a
jiwoo <zbqmgldjfh@gmail.com>
2023-02-23 10:15:47 +0900

    chore: Notification 문구 변경

--- src/main/java/com/kustacks/kuring/common/firebase/FirebaseService.java
+++ src/main/java/com/kustacks/kuring/common/firebase/FirebaseService.java
@@ -23,7 +23,7 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class FirebaseService {
 
-    private static final String NOTIFICATION_TITLE = "신규 공지가 왔어요!";
+    private static final String NOTIFICATION_TITLE = "새로운 공지가 왔어요!";
     private final String DEV_SUFFIX = ".dev";
     private final FirebaseMessaging firebaseMessaging;
     private final ObjectMapper objectMapper;
