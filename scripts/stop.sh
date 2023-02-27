5bdff3cfac1ef87e9a024818e563d5dbb9ca35ee
ngwoon <todory2002@gmail.com>
2023-02-28 04:27:11 +0900

    deploy: codedeploy시 환경변수 인식

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -10,6 +10,8 @@ TIME_NOW=$(date +%c)
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 
+printenv SENTRY_URL
+
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
