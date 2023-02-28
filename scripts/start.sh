8bc6aa2926ccaf5a1aa42e9a6b5fb1525cee26e5
ngwoon <todory2002@gmail.com>
2023-02-28 12:13:53 +0900

    deploy: codedeploy시 환경변수 인식, 서버 기본 포트 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -20,5 +20,4 @@ echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
 nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-printenv SENTRY_URL >> $DEPLOY_LOG
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
