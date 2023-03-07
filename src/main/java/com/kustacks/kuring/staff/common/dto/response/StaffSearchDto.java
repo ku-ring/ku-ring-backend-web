e2a951afef186f4fb9ad14f4bd76dbbc59f19887
jiwoo <zbqmgldjfh@gmail.com>
2023-03-07 22:00:35 +0900

    feat: StaffQueryRepository#findAllByKeywords 구현

---
+++ src/main/java/com/kustacks/kuring/staff/common/dto/response/StaffSearchDto.java
@@ -0,0 +1,36 @@
+package com.kustacks.kuring.staff.common.dto.response;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class StaffSearchDto {
+
+    private String name;
+
+    private String major;
+
+    private String lab;
+
+    private String phone;
+
+    private String email;
+
+    private String deptName;
+
+    private String collegeName;
+
+    @QueryProjection
+    public StaffSearchDto(String name, String major, String lab, String phone, String email, String deptName, String collegeName) {
+        this.name = name;
+        this.major = major;
+        this.lab = lab;
+        this.phone = phone;
+        this.email = email;
+        this.deptName = deptName;
+        this.collegeName = collegeName;
+    }
+}
