2349ae721272647e17ddb73015881c380a247073
jiwoo <zbqmgldjfh@gmail.com>
2023-03-10 13:11:31 +0900

    refactor: NoticeControllerV2 분리

--- src/main/java/com/kustacks/kuring/notice/presentation/NoticeController.java
+++ src/main/java/com/kustacks/kuring/notice/presentation/NoticeController.java
@@ -1,13 +1,9 @@
 package com.kustacks.kuring.notice.presentation;
 
-import com.kustacks.kuring.common.dto.BaseResponse;
 import com.kustacks.kuring.notice.business.NoticeService;
 import com.kustacks.kuring.notice.common.dto.NoticeListResponse;
-import com.kustacks.kuring.notice.common.dto.NoticeLookupResponse;
-import com.kustacks.kuring.search.common.dto.response.NoticeSearchDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import java.util.List;
-
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.NOTICE_SEARCH_SUCCESS;
 
 @Validated
 @RestController
@@ -35,12 +27,5 @@ public class NoticeController {
             @RequestParam(name = "offset") @Min(0) int offset,
             @RequestParam(name = "max") @Min(1) @Max(30) int max) {
         return noticeService.getNotices(type, offset, max);
-    }
-
-    @GetMapping("/api/v2/notices/search")
-    public ResponseEntity<BaseResponse<NoticeLookupResponse>> searchNotice(@NotBlank @RequestParam String content) {
-        List<NoticeSearchDto> noticeDtoList = noticeService.findAllNoticeByContent(content);
-        NoticeLookupResponse response = new NoticeLookupResponse(noticeDtoList);
-        return ResponseEntity.ok().body(new BaseResponse(NOTICE_SEARCH_SUCCESS, response));
     }
 }
