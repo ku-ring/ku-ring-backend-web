b584ea26f69f8a7dd0aa4aedb6ab5c6d037e6e1a
jiwoo <zbqmgldjfh@gmail.com>
2023-03-19 16:17:17 +0900

    feat: ApiClient 인터페이스 제거

--- src/main/java/com/kustacks/kuring/worker/client/ApiClient.java
+++
@@ -1,4 +0,0 @@
-package com.kustacks.kuring.worker.client;
-
-public interface ApiClient {
-}
--- src/main/java/com/kustacks/kuring/worker/client/staff/StaffApiClient.java
+++ src/main/java/com/kustacks/kuring/worker/client/staff/StaffApiClient.java
@@ -1,13 +1,12 @@
 package com.kustacks.kuring.worker.client.staff;
 
 import com.kustacks.kuring.common.error.InternalLogicException;
-import com.kustacks.kuring.worker.client.ApiClient;
 import com.kustacks.kuring.worker.update.staff.deptinfo.DeptInfo;
 import org.jsoup.nodes.Document;
 
 import java.util.List;
 
-public interface StaffApiClient extends ApiClient {
+public interface StaffApiClient {
 
     int SCRAP_TIMEOUT = 10000;
 
