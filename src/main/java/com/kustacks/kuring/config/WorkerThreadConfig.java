4cf30d3569e2ca84d639c3f171456f7cb242cba7
jiwoo <zbqmgldjfh@gmail.com>
2023-03-25 15:19:06 +0900

    fear: WorkerThreadConfig 구현

---
+++ src/main/java/com/kustacks/kuring/config/WorkerThreadConfig.java
@@ -0,0 +1,27 @@
+package com.kustacks.kuring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@EnableAsync
+@Configuration
+public class WorkerThreadConfig {
+
+    @Bean
+    public ThreadPoolTaskExecutor departmentNoticeUpdaterThreadTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setKeepAliveSeconds(10);
+        taskExecutor.setCorePoolSize(5);
+        taskExecutor.setMaxPoolSize(10);
+        taskExecutor.setQueueCapacity(100);
+        taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);
+        taskExecutor.setThreadNamePrefix("DeptNot-updt-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
