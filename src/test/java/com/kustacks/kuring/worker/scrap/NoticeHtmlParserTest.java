8aa3ff84c5624eec4638fc8d3967e2f87f6815bb
jiwoo <zbqmgldjfh@gmail.com>
2023-03-30 21:57:54 +0900

    test: LatestPageNoticeHtmlParser 구현

--- src/test/java/com/kustacks/kuring/worker/scrap/NoticeHtmlParserTest.java
+++ src/test/java/com/kustacks/kuring/worker/scrap/NoticeHtmlParserTest.java
@@ -1,5 +1,6 @@
 package com.kustacks.kuring.worker.scrap;
 
+import com.kustacks.kuring.worker.scrap.parser.notice.LatestPageNoticeHtmlParser;
 import com.kustacks.kuring.worker.scrap.parser.notice.LatestPageNoticeHtmlParserTwo;
 import com.kustacks.kuring.worker.scrap.parser.notice.RowsDto;
 import com.kustacks.kuring.worker.update.notice.dto.response.CommonNoticeFormatDto;
@@ -18,6 +19,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class NoticeHtmlParserTest {
+
+    @DisplayName("LatestPageNoticeHtmlParser 테스트")
+    @Test
+    void computer_science_document_parsing_success() throws IOException {
+        // given
+        Document doc = Jsoup.parse(loadHtmlFile("src/test/resources/notice/cse.html"));
+
+        // when
+        RowsDto rowsDto = new LatestPageNoticeHtmlParser().parse(doc);
+        List<CommonNoticeFormatDto> important = rowsDto.buildImportantRowList("important");
+        List<CommonNoticeFormatDto> normal = rowsDto.buildNormalRowList("normal");
+
+        // then
+        assertAll(
+                () -> assertThat(important.size()).isEqualTo(12),
+                () -> assertThat(normal.size()).isEqualTo(12)
+        );
+    }
 
     @DisplayName("LatestPageNoticeHtmlParserTwo 테스트")
     @Test
