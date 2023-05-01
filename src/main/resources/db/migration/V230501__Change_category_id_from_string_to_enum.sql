e7fe6cf2a4d8c44b1e344c7194fc13831f00fc60
jiwoo <zbqmgldjfh@gmail.com>
2023-05-01 19:18:36 +0900

    feat: Category Id update DDL문 작성

--- src/main/resources/db/migration/V230501__Change_category_id_from_string_to_enum.sql
+++ src/main/resources/db/migration/V230501__Change_category_id_from_string_to_enum.sql
@@ -1,5 +1,5 @@
-SET foreign_key_checks = 0
-UPDATE category SET name = UPPER(name);
+SET SESSION foreign_key_checks = 0;
+UPDATE category SET category.name = UPPER(category.name);
 UPDATE user_category SET category_name = UPPER(category_name);
 UPDATE notice SET category_name = UPPER(category_name);
-SET foreign_key_checks = 1
+SET SESSION foreign_key_checks = 1;
