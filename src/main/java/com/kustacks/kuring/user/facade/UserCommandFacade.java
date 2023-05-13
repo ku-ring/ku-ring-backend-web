4eb5c651da3367b6b4afb6a6373bdf3c3b5b1c9b
jiwoo <zbqmgldjfh@gmail.com>
2023-05-13 13:32:42 +0900

    refactor(UserCommandFacade): 기존의 Service 계층에 있던 구독 로직을 Facade로 올린다

--- src/main/java/com/kustacks/kuring/user/facade/UserCommandFacade.java
+++ src/main/java/com/kustacks/kuring/user/facade/UserCommandFacade.java
@@ -9,7 +9,9 @@ import com.kustacks.kuring.common.firebase.FirebaseService;
 import com.kustacks.kuring.common.firebase.exception.FirebaseSubscribeException;
 import com.kustacks.kuring.common.firebase.exception.FirebaseUnSubscribeException;
 import com.kustacks.kuring.feedback.business.FeedbackService;
+import com.kustacks.kuring.notice.domain.DepartmentName;
 import com.kustacks.kuring.user.business.UserService;
+import com.kustacks.kuring.user.common.dto.SubscribeCompareResultDto;
 import com.kustacks.kuring.user.domain.UserCategory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,7 +43,8 @@ public class UserCommandFacade {
 
     public void editSubscribeDepartments(String userToken, List<String> departments) {
         firebaseService.validationToken(userToken);
-        userService.editSubscribeDepartmentList(userToken, departments);
+        SubscribeCompareResultDto<DepartmentName> compareResults = userService.editSubscribeDepartmentList(userToken, departments);
+        editDepartmentNameList(userToken, compareResults.getSavedNameList(), compareResults.getDeletedNameList());
     }
 
     public void saveFeedback(String userToken, String feedback) {
@@ -54,14 +57,14 @@ public class UserCommandFacade {
             SubscribedRollbackEvent subscribedRollbackEvent = new SubscribedRollbackEvent(userToken);
             Events.raise(subscribedRollbackEvent);
 
-            subscribeUserCategory(compareResult, userToken, subscribedRollbackEvent);
-            unsubscribeUserCategory(compareResult, userToken, subscribedRollbackEvent);
+            subscribeUserCategory(userToken, compareResult, subscribedRollbackEvent);
+            unsubscribeUserCategory(userToken, compareResult, subscribedRollbackEvent);
         } catch (FirebaseSubscribeException | FirebaseUnSubscribeException e) {
             throw new APIException(ErrorCode.API_FB_CANNOT_EDIT_CATEGORY, e);
         }
     }
 
-    private void subscribeUserCategory(Map<String, List<UserCategory>> compareResult, String token, SubscribedRollbackEvent subscribedRollbackEvent) {
+    private void subscribeUserCategory(String token, Map<String, List<UserCategory>> compareResult, SubscribedRollbackEvent subscribedRollbackEvent) {
         List<UserCategory> newUserCategories = compareResult.get(NEW_CATEGORY_FLAG);
         for (UserCategory newUserCategory : newUserCategories) {
             firebaseService.subscribe(token, newUserCategory.getCategoryName());
@@ -71,13 +74,43 @@ public class UserCommandFacade {
         }
     }
 
-    private void unsubscribeUserCategory(Map<String, List<UserCategory>> compareResult, String token, SubscribedRollbackEvent subscribedRollbackEvent) {
+    private void unsubscribeUserCategory(String token, Map<String, List<UserCategory>> compareResult, SubscribedRollbackEvent subscribedRollbackEvent) {
         List<UserCategory> removeUserCategories = compareResult.get(REMOVE_CATEGORY_FLAG);
         for (UserCategory removeUserCategory : removeUserCategories) {
             firebaseService.unsubscribe(token, removeUserCategory.getCategoryName());
             categoryService.deleteUserCategory(removeUserCategory);
             subscribedRollbackEvent.deleteNewCategoryName(removeUserCategory.getCategoryName());
             log.info("구독 취소 = {}", removeUserCategory.getCategoryName());
+        }
+    }
+
+    private void editDepartmentNameList(String userToken, List<DepartmentName> savedDepartmentNames, List<DepartmentName> deletedDepartmentNames) {
+        try {
+            SubscribedRollbackEvent subscribedRollbackEvent = new SubscribedRollbackEvent(userToken);
+            Events.raise(subscribedRollbackEvent);
+
+            subscribeDepartment(userToken, savedDepartmentNames, subscribedRollbackEvent);
+            unsubscribeDepartment(userToken, deletedDepartmentNames, subscribedRollbackEvent);
+        } catch (FirebaseSubscribeException | FirebaseUnSubscribeException e) {
+            throw new APIException(ErrorCode.API_FB_CANNOT_EDIT_CATEGORY, e);
+        }
+    }
+
+    private void subscribeDepartment(String userToken, List<DepartmentName> newDepartmentNames, SubscribedRollbackEvent subscribedRollbackEvent) {
+        for (DepartmentName newDepartmentName : newDepartmentNames) {
+            firebaseService.subscribe(userToken, newDepartmentName.getName());
+            userService.subscribeDepartment(userToken, newDepartmentName);
+            subscribedRollbackEvent.addNewCategoryName(newDepartmentName.getName());
+            log.info("구독 성공 = {}", newDepartmentName.getName());
+        }
+    }
+
+    private void unsubscribeDepartment(String userToken, List<DepartmentName> removeDepartmentNames, SubscribedRollbackEvent subscribedRollbackEvent) {
+        for (DepartmentName removeDepartmentName : removeDepartmentNames) {
+            firebaseService.unsubscribe(userToken, removeDepartmentName.getName());
+            userService.unsubscribeDepartment(userToken, removeDepartmentName);
+            subscribedRollbackEvent.deleteNewCategoryName(removeDepartmentName.getName());
+            log.info("구독 취소 = {}", removeDepartmentName.getName());
         }
     }
 }
