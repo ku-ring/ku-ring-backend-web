ec152bb44fc1527db93fbb86b7ce6712475003fd
jiwoo <zbqmgldjfh@gmail.com>
2023-05-30 21:03:56 +0900

    feat(NoticeQueryRepository): 사용하지 않는 메서드 제거

--- src/main/java/com/kustacks/kuring/notice/domain/NoticeQueryRepository.java
+++ src/main/java/com/kustacks/kuring/notice/domain/NoticeQueryRepository.java
@@ -1,7 +1,6 @@
 package com.kustacks.kuring.notice.domain;
 
 import com.kustacks.kuring.category.domain.Category;
-import com.kustacks.kuring.category.domain.CategoryName;
 import com.kustacks.kuring.notice.common.dto.NoticeDto;
 import com.kustacks.kuring.notice.common.dto.NoticeSearchDto;
 import org.springframework.data.domain.Pageable;
@@ -13,8 +12,4 @@ public interface NoticeQueryRepository {
     List<NoticeDto> findNoticesByCategoryWithOffset(Category category, Pageable pageable);
 
     List<NoticeSearchDto> findAllByKeywords(List<String> containedNames);
-
-    List<String> findNormalArticleIdsByCategory(CategoryName categoryName);
-
-    void deleteAllByIdsAndCategory(CategoryName categoryName, List<String> articleIds);
 }
--- src/main/java/com/kustacks/kuring/notice/domain/NoticeQueryRepositoryImpl.java
+++ src/main/java/com/kustacks/kuring/notice/domain/NoticeQueryRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.kustacks.kuring.notice.domain;
 
 import com.kustacks.kuring.category.domain.Category;
-import com.kustacks.kuring.category.domain.CategoryName;
 import com.kustacks.kuring.notice.common.dto.NoticeDto;
 import com.kustacks.kuring.notice.common.dto.NoticeSearchDto;
 import com.kustacks.kuring.notice.common.dto.QNoticeDto;
@@ -47,29 +46,6 @@ public class NoticeQueryRepositoryImpl implements NoticeQueryRepository {
                 .where(isContainSubject(keywords).or(isContainCategory(keywords)))
                 .orderBy(notice.postedDate.desc())
                 .fetch();
-    }
-
-    @Override
-    public List<String> findNormalArticleIdsByCategory(CategoryName categoryName) {
-        return queryFactory
-                .select(notice.articleId)
-                .from(notice)
-                .where(notice.category.categoryName.eq(categoryName))
-                .orderBy(notice.articleId.asc())
-                .fetch();
-    }
-
-    @Override
-    public void deleteAllByIdsAndCategory(CategoryName categoryName, List<String> articleIds) {
-        if(articleIds.isEmpty()) {
-            return;
-        }
-
-        queryFactory
-                .delete(notice)
-                .where(notice.category.categoryName.eq(categoryName)
-                        .and(notice.articleId.in(articleIds)))
-                .execute();
     }
 
     private static BooleanBuilder isContainSubject(List<String> keywords) {
