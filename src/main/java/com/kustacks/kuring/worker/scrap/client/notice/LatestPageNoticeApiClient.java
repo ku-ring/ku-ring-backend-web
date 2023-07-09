fd49bdd625bd826b1de50adaeec7fb1043ee900f
jiwoo <zbqmgldjfh@gmail.com>
2023-07-09 14:42:03 +0900

    feat(LatestPageNoticeApiClient#requestAll): Reqeust All log 추가

--- src/main/java/com/kustacks/kuring/worker/scrap/client/notice/LatestPageNoticeApiClient.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/client/notice/LatestPageNoticeApiClient.java
@@ -60,6 +60,7 @@ public class LatestPageNoticeApiClient implements NoticeApiClient<ScrapingResult
                 ScrapingResultDto resultDto = getScrapingResultDto(i, deptInfo, totalNoticeSize, LATEST_SCRAP_ALL_TIMEOUT);
                 reqResults.add(resultDto);
             } catch (IOException e) {
+                log.info("requestAll() IOException: {}", e.getMessage());
                 throw new InternalLogicException(ErrorCode.NOTICE_SCRAPER_CANNOT_SCRAP, e);
             } catch (NullPointerException | IndexOutOfBoundsException e) {
                 throw new InternalLogicException(ErrorCode.NOTICE_SCRAPER_CANNOT_PARSE, e);
