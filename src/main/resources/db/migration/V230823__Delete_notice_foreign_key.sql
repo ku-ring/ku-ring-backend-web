0e90a248027ce39857ab12e2127296f4d99c6e04
jiwoo <zbqmgldjfh@gmail.com>
2023-08-23 22:01:07 +0900

    fix: Notice의 CategoryName에 대한 외래키 제약조건이 있는 경우에만 제거 하도록

--- src/main/resources/db/migration/V230823__Delete_notice_foreign_key.sql
+++ src/main/resources/db/migration/V230823__Delete_notice_foreign_key.sql
@@ -1,1 +1,11 @@
-alter table notice drop foreign key FK2pjg4hwditxubnvu938yptsck;
+set @var=if((SELECT true FROM information_schema.TABLE_CONSTRAINTS WHERE
+            CONSTRAINT_SCHEMA = DATABASE() AND
+            TABLE_NAME        = 'notice' AND
+            CONSTRAINT_NAME   = 'FK2pjg4hwditxubnvu938yptsck' AND
+            CONSTRAINT_TYPE   = 'FOREIGN KEY') = true,
+    'alter table notice drop foreign key FK2pjg4hwditxubnvu938yptsck',
+    'select 1');
+
+prepare stmt from @var;
+execute stmt;
+deallocate prepare stmt;
