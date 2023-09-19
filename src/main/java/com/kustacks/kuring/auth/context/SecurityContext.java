457f0dfa019d7c553ba2fdcfa09a985111a9d82c
jiwoo <zbqmgldjfh@gmail.com>
2023-09-19 15:39:54 +0900

    feat(SecurityContextPersistenceFilter): SecurityContextPersistenceFilter, SecurityContext 구현 완료

---
+++ src/main/java/com/kustacks/kuring/auth/context/SecurityContext.java
@@ -0,0 +1,22 @@
+package com.kustacks.kuring.auth.context;
+
+import java.io.Serializable;
+
+public class SecurityContext implements Serializable {
+    private Authentication authentication;
+
+    public SecurityContext() {
+    }
+
+    public SecurityContext(Authentication authentication) {
+        this.authentication = authentication;
+    }
+
+    public void setAuthentication(Authentication authentication) {
+        this.authentication = authentication;
+    }
+
+    public Authentication getAuthentication() {
+        return authentication;
+    }
+}
