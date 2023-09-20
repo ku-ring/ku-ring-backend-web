62f49c2383be81b8a0f7b7cf5f7956decb2ee4e7
jiwoo <zbqmgldjfh@gmail.com>
2023-09-20 14:53:40 +0900

    refactor(JwtTokenProperties): 토큰 key값 수정

--- src/main/java/com/kustacks/kuring/auth/token/JwtTokenProperties.java
+++ src/main/java/com/kustacks/kuring/auth/token/JwtTokenProperties.java
@@ -1,16 +1,19 @@
 package com.kustacks.kuring.auth.token;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
 @ConstructorBinding
-@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "security.jwt.token")
 public class JwtTokenProperties {
 
     private String secretKey;
     private long expireLength;
+
+    public JwtTokenProperties(String secretKey, String expireLength) {
+        this.secretKey = secretKey;
+        this.expireLength = Long.parseLong(expireLength);
+    }
 }
