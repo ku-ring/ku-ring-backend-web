0cca2a9f97a2cdf512ef3aaff3e16e67bcb9538d
jiwoo <zbqmgldjfh@gmail.com>
2023-09-27 22:44:59 +0900

    feat(AuthenticationPrincipal): Authenti cationPrincipal ArgumentResolver 구현

---
+++ src/main/java/com/kustacks/kuring/auth/authorization/AuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,26 @@
+package com.kustacks.kuring.auth.authorization;
+
+import com.kustacks.kuring.auth.context.Authentication;
+import com.kustacks.kuring.auth.context.SecurityContextHolder;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+    }
+
+    @Override
+    public Authentication resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            throw new IllegalStateException("SecurityContext is empty");
+        }
+
+        return authentication;
+    }
+}
