139a8c28d617ec23ec689ac7f322db3b5eaf2695
jiwoo <zbqmgldjfh@gmail.com>
2024-01-12 20:40:36 +0900

    feat(Url): Url의 pattern 변경과 상수로 미리 compile해 두도록 변경

--- src/main/java/com/kustacks/kuring/notice/domain/Url.java
+++ src/main/java/com/kustacks/kuring/notice/domain/Url.java
@@ -16,7 +16,38 @@ import java.util.regex.Pattern;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Url {
 
-    private static final String REGEX_PATTERN = "^((http|https)://)?([a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?)$";
+    /**
+     * Regular Expression by RFC 3986 for URI Validation
+     * @link https://datatracker.ietf.org/doc/html/rfc3986#appendix-B
+     * @author jiwoo
+     */
+    private static final String REGEX_SCHEME = "[A-Za-z][+-.\\w^_]*:";
+
+    // Example: "//".
+    private static final String REGEX_AUTHORATIVE_DECLARATION = "/{2}";
+
+    // Optional component. Example: "suzie:abc123@". The use of the format "user:password" is deprecated.
+    private static final String REGEX_USERINFO = "(?:\\S+(?::\\S*)?@)?";
+
+    // Examples: "fitbit.com", "22.231.113.64".
+    private static final String REGEX_HOST = "(?:" +  // @Author = http://www.regular-expressions.info/examples.html
+            // IP address
+            "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" +
+            "|" +  // host name
+            "(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)" +  // domain name
+            "(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*" +  // TLD identifier must have >= 2 characters
+            "(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))";
+
+    // Example: ":8042".
+    private static final String REGEX_PORT = "(?::\\d{2,5})?";
+
+    //Example: "/user/heartrate?foo=bar#element1".
+    private static final String REGEX_RESOURCE_PATH = "(?:/\\S*)?";
+
+    private static final String REGEX_URL = "^(?:(?:" + REGEX_SCHEME + REGEX_AUTHORATIVE_DECLARATION + ")?" +
+            REGEX_USERINFO + REGEX_HOST + REGEX_PORT + REGEX_RESOURCE_PATH + ")$";
+
+    private static final Pattern compiledUrlPattern = Pattern.compile(REGEX_URL);
 
     @Column(name = "url", length = 256, nullable = false)
     private String value;
@@ -30,11 +61,11 @@ public class Url {
     }
 
     private boolean isValidUrl(String fullUrl) {
-        return !Objects.isNull(fullUrl) && patternMatches(fullUrl, REGEX_PATTERN);
+        return !Objects.isNull(fullUrl) && patternMatches(fullUrl);
     }
 
-    private boolean patternMatches(String targetUrl, String regexPattern) {
-        return Pattern.compile(regexPattern)
+    private boolean patternMatches(String targetUrl) {
+        return compiledUrlPattern
                 .matcher(targetUrl)
                 .matches();
     }
