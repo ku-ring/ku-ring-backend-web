f7e9215eeeb95cab78ed58f1be699994ddf17894
jiwoo <zbqmgldjfh@gmail.com>
2024-01-25 16:33:37 +0900

    test(HexagonalArchitecture): User의 HexagonalArchitecture 검증

---
+++ src/test/java/com/kustacks/kuring/archunit/DependencyRuleTests.java
@@ -0,0 +1,46 @@
+package com.kustacks.kuring.archunit;
+
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class DependencyRuleTests {
+
+	@DisplayName("User 헥사고갈 아키텍처 검증")
+	@Test
+	void validateUserArchitecture() {
+		HexagonalArchitecture.boundedContext("com.kustacks.kuring.user")
+
+				.withDomainLayer("domain")
+
+				.withAdaptersLayer("adapter")
+				.incoming("in.web")
+				.outgoing("out.persistence")
+				.and()
+
+				.withApplicationLayer("application")
+				.services("service")
+				.incomingPorts("port.in")
+				.outgoingPorts("port.out")
+				.and()
+
+				.withConfiguration("configuration")
+				.check(new ClassFileImporter()
+						.importPackages("com.kustacks.kuring.user.."));
+	}
+
+	@Test
+	void testPackageDependencies() {
+		noClasses()
+				.that()
+				.resideInAPackage("com.kustacks.kuring.user.domain..")
+				.should()
+				.dependOnClassesThat()
+				.resideInAnyPackage("com.kustacks.kuring.user.application..")
+				.check(new ClassFileImporter()
+						.importPackages("com.kustacks.kuring.user.."));
+	}
+
+}
