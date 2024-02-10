fec3b594cbda820412dbc24051b05a45fc9533e2
jiwoo <zbqmgldjfh@gmail.com>
2024-02-10 16:43:26 +0900

    feat: API 문서화 완료

--- src/main/java/com/kustacks/kuring/admin/adapter/in/web/AdminCommandApiV2.java
+++ src/main/java/com/kustacks/kuring/admin/adapter/in/web/AdminCommandApiV2.java
@@ -1,23 +1,31 @@
 package com.kustacks.kuring.admin.adapter.in.web;
 
-import com.kustacks.kuring.admin.application.port.in.dto.RealNotificationCommand;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.ADMIN_REAL_NOTICE_CREATE_SUCCESS;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.ADMIN_TEST_NOTICE_CREATE_SUCCESS;
+
 import com.kustacks.kuring.admin.adapter.in.web.dto.RealNotificationRequest;
 import com.kustacks.kuring.admin.adapter.in.web.dto.TestNotificationRequest;
 import com.kustacks.kuring.admin.application.port.in.AdminCommandUseCase;
+import com.kustacks.kuring.admin.application.port.in.dto.RealNotificationCommand;
 import com.kustacks.kuring.admin.domain.AdminRole;
 import com.kustacks.kuring.auth.authorization.AuthenticationPrincipal;
 import com.kustacks.kuring.auth.context.Authentication;
 import com.kustacks.kuring.auth.secured.Secured;
 import com.kustacks.kuring.common.dto.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.ADMIN_REAL_NOTICE_CREATE_SUCCESS;
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.ADMIN_TEST_NOTICE_CREATE_SUCCESS;
-
+@Tag(name = "Admin-Command", description = "관리자가 주체가 되는 정보 수정")
 @Validated
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +34,8 @@ public class AdminCommandApiV2 {
 
     private final AdminCommandUseCase adminCommandUseCase;
 
+    @Operation(summary = "테스트 공지 전송", description = "테스트 공지를 전송합니다, 실제 운영시 사용하지 않습니다")
+    @SecurityRequirement(name = "JWT")
     @Secured(AdminRole.ROLE_ROOT)
     @PostMapping("/notices/dev")
     public ResponseEntity<BaseResponse<String>> createTestNotice(
@@ -35,6 +45,8 @@ public class AdminCommandApiV2 {
         return ResponseEntity.ok().body(new BaseResponse<>(ADMIN_TEST_NOTICE_CREATE_SUCCESS, null));
     }
 
+    @Operation(summary = "전체 공지 전송", description = "전체 공지를 전송합니다, 실제 운영시 사용합니다")
+    @SecurityRequirement(name = "JWT")
     @Secured(AdminRole.ROLE_ROOT)
     @PostMapping("/notices/prod")
     public ResponseEntity<BaseResponse<String>> createRealNotice(
