1a44785e7c3624100f56809ac14f8aa99666ab95
jiwoo <zbqmgldjfh@gmail.com>
2024-02-21 23:10:02 +0900

    refactor(RealEstateNoticeHtmlParser): RealEstateNoticeHtmlParser 리팩토링

--- src/main/java/com/kustacks/kuring/worker/scrap/parser/notice/RealEstateNoticeHtmlParser.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/parser/notice/RealEstateNoticeHtmlParser.java
@@ -1,7 +1,7 @@
 package com.kustacks.kuring.worker.scrap.parser.notice;
 
-import com.kustacks.kuring.common.exception.code.ErrorCode;
 import com.kustacks.kuring.common.exception.InternalLogicException;
+import com.kustacks.kuring.common.exception.code.ErrorCode;
 import com.kustacks.kuring.worker.scrap.deptinfo.DeptInfo;
 import com.kustacks.kuring.worker.scrap.deptinfo.real_estate.RealEstateDept;
 import org.jsoup.nodes.Document;
@@ -13,7 +13,6 @@ import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
 @Component
@@ -26,28 +25,33 @@ public class RealEstateNoticeHtmlParser implements NoticeHtmlParser {
 
     @Override
     public RowsDto parse(Document document) {
-        List<String[]> result = new LinkedList<>();
-        Elements rows = document.select(".board_list > table > tbody > tr");
         try {
-            for (Element row : rows) {
-                String[] oneNoticeInfo = new String[3];
-                Elements tds = row.getElementsByTag("td");
-
-                Element subjectAndIdElement = tds.get(2).getElementsByTag("a").get(0);
-
-                UriComponents hrefUri = UriComponentsBuilder.fromUriString(subjectAndIdElement.attr("href")).build();
-                MultiValueMap<String, String> queryParams = hrefUri.getQueryParams();
-
-                oneNoticeInfo[0] = queryParams.get("wr_id").get(0);
-                oneNoticeInfo[1] = tds.get(3).ownText();
-                oneNoticeInfo[2] = subjectAndIdElement.ownText();
-
-                result.add(oneNoticeInfo);
-            }
+            Elements rows = document.select(".board_list > table > tbody > tr");
+            List<String[]> normalRowList = extractNoticeListFromRows(rows);
+            return new RowsDto(Collections.emptyList(), normalRowList);
         } catch (NullPointerException | IndexOutOfBoundsException e) {
             throw new InternalLogicException(ErrorCode.NOTICE_SCRAPER_CANNOT_PARSE, e);
         }
+    }
 
-        return new RowsDto(Collections.emptyList(), result);
+    private List<String[]> extractNoticeListFromRows(Elements rows) {
+        return rows.stream()
+                .map(RealEstateNoticeHtmlParser::extractNoticeFromRow)
+                .toList();
+    }
+
+    private static String[] extractNoticeFromRow(Element row) {
+        String[] oneNoticeInfo = new String[3];
+        Elements tds = row.getElementsByTag("td");
+
+        Element subjectAndIdElement = tds.get(2).getElementsByTag("a").get(0);
+
+        UriComponents hrefUri = UriComponentsBuilder.fromUriString(subjectAndIdElement.attr("href")).build();
+        MultiValueMap<String, String> queryParams = hrefUri.getQueryParams();
+
+        oneNoticeInfo[0] = queryParams.get("wr_id").get(0);
+        oneNoticeInfo[1] = tds.get(3).ownText();
+        oneNoticeInfo[2] = subjectAndIdElement.ownText();
+        return oneNoticeInfo;
     }
 }
