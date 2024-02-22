62a4a58eedf44c56957129b0e240d2fbca485d68
jiwoo <zbqmgldjfh@gmail.com>
2024-02-22 10:05:52 +0900

    feat(HtmlParser): 사용하지 않는 인터페이스 제거

--- src/main/java/com/kustacks/kuring/worker/scrap/parser/notice/NoticeHtmlParser.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/parser/notice/NoticeHtmlParser.java
@@ -2,10 +2,9 @@ package com.kustacks.kuring.worker.scrap.parser.notice;
 
 import com.kustacks.kuring.common.exception.InternalLogicException;
 import com.kustacks.kuring.worker.scrap.deptinfo.DeptInfo;
-import com.kustacks.kuring.worker.scrap.parser.HtmlParser;
 import org.jsoup.nodes.Document;
 
-public interface NoticeHtmlParser extends HtmlParser {
+public interface NoticeHtmlParser {
 
     boolean support(DeptInfo deptInfo);
 
--- src/main/java/com/kustacks/kuring/worker/scrap/parser/staff/StaffHtmlParser.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/parser/staff/StaffHtmlParser.java
@@ -2,12 +2,11 @@ package com.kustacks.kuring.worker.scrap.parser.staff;
 
 import com.kustacks.kuring.common.exception.InternalLogicException;
 import com.kustacks.kuring.worker.scrap.deptinfo.DeptInfo;
-import com.kustacks.kuring.worker.scrap.parser.HtmlParser;
 import org.jsoup.nodes.Document;
 
 import java.util.List;
 
-public interface StaffHtmlParser extends HtmlParser {
+public interface StaffHtmlParser {
 
     boolean support(DeptInfo deptInfo);
 
