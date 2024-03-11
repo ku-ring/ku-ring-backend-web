3fc9126e2c29b41f97ab3f648b8a93773694107f
jiwoo <zbqmgldjfh@gmail.com>
2024-03-11 20:43:30 +0900

    refactor(KuisNoticeUpdater): CategoryNoticeUpdater에서 KuisNoticeUpdater 로 이름 변경

--- src/main/java/com/kustacks/kuring/worker/scrap/client/notice/KuisHomepageNoticeApiClient.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/client/notice/KuisHomepageNoticeApiClient.java
@@ -68,7 +68,7 @@ public class KuisHomepageNoticeApiClient implements NoticeApiClient<ScrapingResu
             totalNoticeSizeElement = document.selectFirst(".total_count");
         }
 
-        if(totalNoticeSizeElement == null) { // 총 공지 개수가 없는 경우 3000개로 가정
+        if(totalNoticeSizeElement == null) { // 총 공지 개수가 없는 경우 650개로 가정
             return TOTAL_KUIS_NOTICES_COUNT;
         }
 
