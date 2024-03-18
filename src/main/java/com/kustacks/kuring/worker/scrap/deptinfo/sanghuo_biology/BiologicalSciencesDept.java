0b0c0adf2b20faa0ec5b1d2862b8119504f9db21
JiWoo <zbqmgldjfh@gmail.com>
2024-03-18 20:37:48 +0900

    [Feat] 생명과학 특성학과 지원 (#166)

--- src/main/java/com/kustacks/kuring/worker/scrap/deptinfo/sanghuo_biology/BiologicalSciencesDept.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/deptinfo/sanghuo_biology/BiologicalSciencesDept.java
@@ -11,8 +11,7 @@ import java.util.List;
 
 import static com.kustacks.kuring.notice.domain.DepartmentName.BIO_SCIENCE;
 
-// 생명과학특성학과의 baseUrl = http://www.konkuk.ac.kr/cms/Common/MessageBoard/ArticleList.do
-// 생명과학특성학과는 게시글 링크를 비공개로 해놔서 파싱할 수가 없음. 그래서 forumIds를 주석처리해둠.
+
 @RegisterDepartmentMap(key = BIO_SCIENCE)
 public class BiologicalSciencesDept extends SanghuoBiologyCollege {
 
