8229e8e846b6846bfb17449baf6b7896895fc604
jiwoo <zbqmgldjfh@gmail.com>
2024-07-11 10:19:39 +0900

    fix(InMemoryQueryAiModelAdapter): InMemory model의 응답 수정

--- src/main/java/com/kustacks/kuring/ai/adapter/out/model/InMemoryQueryAiModelAdapter.java
+++ src/main/java/com/kustacks/kuring/ai/adapter/out/model/InMemoryQueryAiModelAdapter.java
@@ -4,15 +4,11 @@ import com.kustacks.kuring.ai.application.port.out.QueryAiModelPort;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.document.Document;
-import org.springframework.ai.reader.TextReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
-
-import java.util.List;
 
 @Slf4j
 @Component
@@ -26,23 +22,13 @@ public class InMemoryQueryAiModelAdapter implements QueryAiModelPort {
     @Override
     public Flux<String> call(Prompt prompt) {
         if (prompt.getContents().contains("교내,외 장학금 및 학자금 대출 관련 전화번호들을 안내를 해줘")) {
-            return Flux.just("학생복지처 ", "장학복지팀의 ", "전화번호는 ", "02-450-3211~2이며, ",
-                    "건국사랑/장학사정관장학/기금장학과 ", "관련된 ", "문의는 02-450-3967로 하시면 됩니다.");
-        }
-
-        if (prompt.getContents().contains("2학기 등록금 납부일을 알려줘")) {
-            return Flux.just("1차 등록일은", "2024. 8. 19.(월) 09:00 ~ 8. 23.(금) 16:00",
-                    "2차 등록일은", "2024. 9. 2.(월) 09:00 ~ 9. 6.(금) 16:00"
+            return Flux.just("학","생","복","지","처"," ","장","학","복","지","팀","의",
+                    " ","전","화","번","호","는"," ","0","2","-","4","5","0","-","3","2","1",
+                    "1","~","2","이","며",","," ","건","국","사","랑","/","장","학","사","정",
+                    "관","장","학","/","기","금","장","학","과"," ","관","련","된"," ","문","의",
+                    "는"," ","0","2","-","4","5","0","-","3","9","6","7","로"," ","하","시",
+                    "면"," ","됩","니","다","."
             );
-        }
-
-        if (prompt.getContents().contains("긴 응답 테스트")) {
-            TextReader textReader = new TextReader(kuUniRegisterInfo);
-            List<Document> read = textReader.read();
-
-            return Flux.fromIterable(read)
-                    .flatMap(document -> Flux.just(document.getContent()))
-                    .onErrorStop();
         }
 
         return Flux.just("미리 준비된 테스트 질문이 아닙니다");
