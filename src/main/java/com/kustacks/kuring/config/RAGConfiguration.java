937e2333e0522533d931f58dcb48f47988a32c4f
jiwoo <zbqmgldjfh@gmail.com>
2024-07-17 15:19:00 +0900

    test: ChromaDB test container 설정

--- src/main/java/com/kustacks/kuring/config/RAGConfiguration.java
+++ src/main/java/com/kustacks/kuring/config/RAGConfiguration.java
@@ -1,16 +1,19 @@
 package com.kustacks.kuring.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chroma.ChromaApi;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.ChromaVectorStore;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.Resource;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -18,7 +21,6 @@ import java.nio.file.Paths;
 import java.util.List;
 
 @Slf4j
-@Profile("local")
 @Configuration
 public class RAGConfiguration {
 
@@ -28,6 +30,20 @@ public class RAGConfiguration {
     @Value("vectorstore.json")
     private String vectorStoreName;
 
+    @Profile("test")
+    @Bean
+    public ChromaApi chromaApi(RestTemplate restTemplate) {
+        String chromaUrl = "http://127.0.0.1:8000";
+        return new ChromaApi(chromaUrl, restTemplate);
+    }
+
+    @Profile("test")
+    @Bean
+    public ChromaVectorStore chromaVectorStore(EmbeddingModel embeddingModel, ChromaApi chromaApi) {
+        return new ChromaVectorStore(embeddingModel, chromaApi, false);
+    }
+
+    @Profile("local")
     @Bean
     public SimpleVectorStore simpleVectorStore(EmbeddingModel embeddingModel) {
         SimpleVectorStore simpleVectorStore = new SimpleVectorStore(embeddingModel);
