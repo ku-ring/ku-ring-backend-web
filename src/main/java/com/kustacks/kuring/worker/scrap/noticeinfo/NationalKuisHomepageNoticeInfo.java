360b0783f4b2047b5d4ab63acc3b0f42b671940e
jiwoo <zbqmgldjfh@gmail.com>
2024-07-18 21:15:17 +0900

    feat(KuisHomepageNoticeInfo): textParser 의존성 추가

--- src/main/java/com/kustacks/kuring/worker/scrap/noticeinfo/NationalKuisHomepageNoticeInfo.java
+++ src/main/java/com/kustacks/kuring/worker/scrap/noticeinfo/NationalKuisHomepageNoticeInfo.java
@@ -1,6 +1,7 @@
 package com.kustacks.kuring.worker.scrap.noticeinfo;
 
 import com.kustacks.kuring.notice.domain.CategoryName;
+import com.kustacks.kuring.worker.parser.notice.KuisHomepageNoticeTextParser;
 import com.kustacks.kuring.worker.scrap.client.notice.KuisHomepageNoticeApiClient;
 import com.kustacks.kuring.worker.scrap.client.notice.property.KuisHomepageNoticeProperties;
 import com.kustacks.kuring.worker.parser.notice.KuisHomepageNoticeHtmlParser;
@@ -11,10 +12,12 @@ public class NationalKuisHomepageNoticeInfo extends KuisHomepageNoticeInfo {
     public NationalKuisHomepageNoticeInfo(
             KuisHomepageNoticeApiClient kuisHomepageNoticeApiClient,
             KuisHomepageNoticeHtmlParser kuisHomepageNoticeHtmlParser,
+            KuisHomepageNoticeTextParser kuisHomepageNoticeTextParser,
             KuisHomepageNoticeProperties kuisHomepageNoticeProperties
     ) {
         this.noticeApiClient = kuisHomepageNoticeApiClient;
         this.htmlParser = kuisHomepageNoticeHtmlParser;
+        this.textParser = kuisHomepageNoticeTextParser;
         this.kuisHomepageNoticeProperties = kuisHomepageNoticeProperties;
         this.siteId = 237;
         this.categoryName = CategoryName.NATIONAL;
