d00db858a8f11bcf9e55df644d3598b70f2ec1b8
jiwoo <zbqmgldjfh@gmail.com>
2024-07-21 21:57:03 +0900

    feat(RAGQueryApiV2): RAGQueryApi 문서화

--- src/main/java/com/kustacks/kuring/ai/adapter/in/web/RAGQueryApiV2.java
+++ src/main/java/com/kustacks/kuring/ai/adapter/in/web/RAGQueryApiV2.java
@@ -3,7 +3,9 @@ package com.kustacks.kuring.ai.adapter.in.web;
 import com.kustacks.kuring.ai.application.port.in.RAGQueryUseCase;
 import com.kustacks.kuring.common.annotation.RestWebAdapter;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
+@Tag(name = "AI-Query", description = "AI Assistant")
 @RequiredArgsConstructor
 @RestWebAdapter(path = "/api/v2/ai/messages")
 public class RAGQueryApiV2 {
@@ -20,10 +23,10 @@ public class RAGQueryApiV2 {
     private final RAGQueryUseCase ragQueryUseCase;
 
     @Operation(summary = "사용자 AI에 질문요청", description = "사용자가 궁금한 학교 정보를 AI에게 질문합니다.")
-    @SecurityRequirement(name = "User-Token")
+    @SecurityRequirement(name = USER_TOKEN_HEADER_KEY)
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> askAIQuery(
-            @RequestParam("question") String question,
+            @Parameter(description = "사용자 질문") @RequestParam("question") String question,
             @RequestHeader(USER_TOKEN_HEADER_KEY) String id
     ) {
         return ragQueryUseCase.askAiModel(question, id);
