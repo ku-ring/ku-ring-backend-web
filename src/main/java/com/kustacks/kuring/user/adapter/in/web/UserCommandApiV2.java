736e835d5d262ed4200024dcfe861e61f08c3f8c
jiwoo <zbqmgldjfh@gmail.com>
2024-07-22 19:39:18 +0900

    refactor: SecurityRequirement에서 상수를 사용하도록 변경

--- src/main/java/com/kustacks/kuring/user/adapter/in/web/UserCommandApiV2.java
+++ src/main/java/com/kustacks/kuring/user/adapter/in/web/UserCommandApiV2.java
@@ -1,10 +1,5 @@
 package com.kustacks.kuring.user.adapter.in.web;
 
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.BOOKMAKR_SAVE_SUCCESS;
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.CATEGORY_SUBSCRIBE_SUCCESS;
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.DEPARTMENTS_SUBSCRIBE_SUCCESS;
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.FEEDBACK_SAVE_SUCCESS;
-
 import com.kustacks.kuring.common.annotation.RestWebAdapter;
 import com.kustacks.kuring.common.dto.BaseResponse;
 import com.kustacks.kuring.user.adapter.in.web.dto.UserBookmarkRequest;
@@ -29,6 +24,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.*;
+
 @Tag(name = "User-Command", description = "사용자가 주체가 되는 정보 수정")
 @Slf4j
 @Validated
@@ -41,7 +38,7 @@ class UserCommandApiV2 {
     private final UserCommandUseCase userCommandUseCase;
 
     @Operation(summary = "사용자 카테고리 수정", description = "사용자가 구독한 카테고리 목록을 추가, 삭제 합니다")
-    @SecurityRequirement(name = "User-Token")
+    @SecurityRequirement(name = USER_TOKEN_HEADER_KEY)
     @PostMapping(value = "/subscriptions/categories", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BaseResponse<Void>> editUserSubscribeCategories(
             @Valid @RequestBody UserCategoriesSubscribeRequest request,
@@ -52,7 +49,7 @@ class UserCommandApiV2 {
     }
 
     @Operation(summary = "사용자 학과 수정", description = "사용자가 구독한 학과 목록을 추가, 삭제 합니다")
-    @SecurityRequirement(name = "User-Token")
+    @SecurityRequirement(name = USER_TOKEN_HEADER_KEY)
     @PostMapping(value = "/subscriptions/departments", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<BaseResponse<Void>> editUserSubscribeDepartments(
             @Valid @RequestBody UserDepartmentsSubscribeRequest request,
@@ -63,7 +60,7 @@ class UserCommandApiV2 {
     }
 
     @Operation(summary = "사용자 피드백 작성", description = "사용자가 피드백을 작성하여 저장합니다")
-    @SecurityRequirement(name = "User-Token")
+    @SecurityRequirement(name = USER_TOKEN_HEADER_KEY)
     @PostMapping("/feedbacks")
     public ResponseEntity<BaseResponse<Void>> saveFeedback(
             @Valid @RequestBody UserFeedbackRequest request,
@@ -74,7 +71,7 @@ class UserCommandApiV2 {
     }
 
     @Operation(summary = "사용자 북마크 작성", description = "사용자가 원하는 공지를 북마크 하여 저장합니다")
-    @SecurityRequirement(name = "User-Token")
+    @SecurityRequirement(name = USER_TOKEN_HEADER_KEY)
     @PostMapping("/bookmarks")
     public ResponseEntity<BaseResponse<Void>> saveBookmark(
             @Valid @RequestBody UserBookmarkRequest request,
