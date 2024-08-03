713a40d2dad5901608f3c2253b69ffa0bfeeb7fb
jiwoo <zbqmgldjfh@gmail.com>
2024-08-03 20:20:39 +0900

    fix(StringToDateTimeConverter): 밀리초가 있는 경우 정상 파싱되지 않던 케이스 수정

--- src/main/java/com/kustacks/kuring/common/utils/converter/StringToDateTimeConverter.java
+++ src/main/java/com/kustacks/kuring/common/utils/converter/StringToDateTimeConverter.java
@@ -9,21 +9,35 @@ public class StringToDateTimeConverter {
 
     private static final String REGEX_DATE_TIME = "^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$";
     private static final Pattern compiledDateTimePattern = Pattern.compile(REGEX_DATE_TIME);
+
+    private static final String REGEX_DATE_TIME_T = "^\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(\\.\\d{1,6})?$";
+    private static final Pattern compiledDateTimeTPattern = Pattern.compile(REGEX_DATE_TIME_T);
+
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.KOREA);
+    private static final DateTimeFormatter dateTimeTFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS").withLocale(Locale.KOREA);
 
     public static LocalDateTime convert(String dateTime) {
         if (dateTime == null || dateTime.isBlank()) {
             throw new IllegalArgumentException("Invalid date time format: " + dateTime);
         }
 
-        if (isValidDateTime(dateTime)) {
+        if (isDateTime(dateTime)) {
             return LocalDateTime.parse(dateTime, dateTimeFormatter);
+        }
+
+        if (isDateTimeT(dateTime)) {
+            LocalDateTime parsed = LocalDateTime.parse(dateTime, dateTimeTFormatter);
+            return parsed.withNano(0);
         }
 
         throw new IllegalArgumentException("Invalid date time format: " + dateTime);
     }
 
-    private static boolean isValidDateTime(String dateTime) {
+    private static boolean isDateTime(String dateTime) {
         return compiledDateTimePattern.matcher(dateTime).matches();
+    }
+
+    private static boolean isDateTimeT(String dateTime) {
+        return compiledDateTimeTPattern.matcher(dateTime).matches();
     }
 }
--- src/test/java/com/kustacks/kuring/common/utils/converter/StringToDateTimeConverterTest.java
+++ src/test/java/com/kustacks/kuring/common/utils/converter/StringToDateTimeConverterTest.java
@@ -2,11 +2,13 @@ package com.kustacks.kuring.common.utils.converter;
 
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.time.LocalDateTime;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -14,17 +16,14 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class StringToDateTimeConverterTest {
 
     @DisplayName("날짜 시간이 yyyy-MM-dd HH:mm:ss 형태로 주어지면 LocalDateTime 으로 변환한다")
-    @Test
-    void date_time_test() {
-        // given
-        String stringDate = "2023-04-03 00:00:12";
-        LocalDateTime localDateTime = LocalDateTime.of(2023, 4, 3, 0, 0, 12);
-
+    @ParameterizedTest
+    @MethodSource("stringLocalDateInputProvider")
+    void date_time_test(String stringDate, LocalDateTime expected) {
         // when
         LocalDateTime convertedDateTime = StringToDateTimeConverter.convert(stringDate);
 
         // then
-        assertThat(convertedDateTime).isEqualTo(localDateTime);
+        assertThat(convertedDateTime).isEqualTo(expected);
     }
 
     @DisplayName("지정된 형식의 날짜 시간 구조가 아닌 경우 예외가 발생한다")
@@ -38,5 +37,12 @@ class StringToDateTimeConverterTest {
         // then
         assertThatThrownBy(actual)
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> stringLocalDateInputProvider() {
+        return Stream.of(
+                Arguments.of("2023-04-03 00:00:12", LocalDateTime.of(2023, 4, 3, 0, 0, 12)),
+                Arguments.of("2024-08-03T20:01:27.454996", LocalDateTime.of(2024, 8, 3, 20, 1, 27))
+        );
     }
 }
