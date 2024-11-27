7d1e6db2616c22a493edcfe744c3bd8037054522
Han Ju Kim <hjkim2902@gmail.com>
2024-11-28 00:56:33 +0900

    fix : 부동산 학과 파싱로직 변경

--- src/main/java/com/kustacks/kuring/worker/parser/staff/RealEstateStaffHtmlParser.java
+++ src/main/java/com/kustacks/kuring/worker/parser/staff/RealEstateStaffHtmlParser.java
@@ -7,6 +7,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class RealEstateStaffHtmlParser extends StaffHtmlParserTemplate {
 
@@ -14,23 +17,20 @@ public class RealEstateStaffHtmlParser extends StaffHtmlParserTemplate {
     public boolean support(DeptInfo deptInfo) {
         return deptInfo instanceof RealEstateDept;
     }
-
     protected Elements selectStaffInfoRows(Document document) {
-        Element table = document.select(".sub0201_list").get(0).getElementsByTag("ul").get(0);
-        return table.getElementsByTag("li");
+        return document.select(".row");
     }
 
     protected String[] extractStaffInfoFromRow(Element row) {
-        Element content = row.select(".con").get(0);
+        String name = row.select(".info .title .name").text();
 
-        String name = content.select("dl > dt > a > strong").get(0).text();
-        String major = String.valueOf(content.select("dl > dd").get(0).childNode(4)).replaceFirst("\\s", "").trim();
-
-        Element textMore = content.select(".text_more").get(0);
-
-        String lab = String.valueOf(textMore.childNode(4)).split(":")[1].replaceFirst("\\s", "").trim();
-        String phone = String.valueOf(textMore.childNode(6)).split(":")[1].replaceFirst("\\s", "").trim();
-        String email = textMore.getElementsByTag("a").get(0).text();
-        return new String[]{name, major, lab, phone, email};
+        Elements detalTagElement = row.select(".detail");
+        String jobPosition = detalTagElement.select("dt:contains(직위) + dd").text();
+        String major = detalTagElement.select("dt:contains(연구분야) + dd").text().trim();
+        String lab = detalTagElement.select("dt:contains(연구실) + dd").text().trim();
+        String extensionNumber = detalTagElement.select("dt:contains(연락처) + dd").text().trim();
+        String email = detalTagElement.select("dt:contains(이메일) + dd").text().trim();
+        return new String[]{name, jobPosition, major, lab, extensionNumber, email};
     }
 }
+
