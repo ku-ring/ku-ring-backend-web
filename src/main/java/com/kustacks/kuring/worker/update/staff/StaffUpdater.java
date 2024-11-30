2b36510f54b8d6a5f1b6dedc8dd844244c71c63a
Han Ju Kim <hjkim2902@gmail.com>
2024-11-30 15:16:08 +0900

    feat : 수의과대학 교직원 스크랩시 수의예과만 스크랩 하도록 변경

--- src/main/java/com/kustacks/kuring/worker/update/staff/StaffUpdater.java
+++ src/main/java/com/kustacks/kuring/worker/update/staff/StaffUpdater.java
@@ -69,7 +69,6 @@ public class StaffUpdater {
 
     private Map<String, StaffDto> convertStaffDtoMap(List<StaffDto> scrapedStaffDtos) {
         return scrapedStaffDtos.stream()
-                .distinct()
                 .collect(Collectors.toMap(StaffDto::identifier, staffDto -> staffDto));
     }
 
