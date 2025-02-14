75e1b8c5b0b907f1250df7ccc6f3f0a03972b219
Han Ju Kim <hjkim2902@gmail.com>
2025-02-14 14:37:50 +0900

    feat : MimeMessage 생성 책임 이동

--- src/main/java/com/kustacks/kuring/email/application/port/in/EmailCommandUseCase.java
+++ src/main/java/com/kustacks/kuring/email/application/port/in/EmailCommandUseCase.java
@@ -1,8 +1,5 @@
 package com.kustacks.kuring.email.application.port.in;
 
-import com.kustacks.kuring.email.application.port.in.dto.EmailSendCommand;
-
 public interface EmailCommandUseCase {
     void sendVerificationEmail(String email);
-    void sendEmail(EmailSendCommand command);
 }
--- src/main/java/com/kustacks/kuring/email/application/service/EmailCommandService.java
+++ src/main/java/com/kustacks/kuring/email/application/service/EmailCommandService.java
@@ -3,58 +3,69 @@ package com.kustacks.kuring.email.application.service;
 import com.kustacks.kuring.common.annotation.UseCase;
 import com.kustacks.kuring.common.exception.code.ErrorCode;
 import com.kustacks.kuring.email.application.port.in.EmailCommandUseCase;
-import com.kustacks.kuring.email.application.port.in.dto.EmailSendCommand;
 import com.kustacks.kuring.email.application.port.out.EmailClientPort;
-import com.kustacks.kuring.email.application.port.out.EmailEventPort;
+import com.kustacks.kuring.email.application.port.out.TemplateEnginePort;
 import com.kustacks.kuring.email.application.port.out.VerificationCodeCommandPort;
 import com.kustacks.kuring.email.application.service.exception.EmailBusinessException;
+import jakarta.mail.MessagingException;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.annotation.Transactional;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.Context;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @Slf4j
 @UseCase
 @RequiredArgsConstructor
 public class EmailCommandService implements EmailCommandUseCase {
-    private final EmailEventPort emailEventPort;
-    private final TemplateEngine templateEngine;
+    private final TemplateEnginePort templateEnginePort;
     private final EmailClientPort emailClientPort;
     private final VerificationCodeCommandPort verificationCodeCommandPort;
 
-    @Transactional
+    private static final String TEMPLATE_FILE = "mail/email-verification";
+    private static final String FROM_EMAIL = "no-reply@ku-ring.com";
+    private static final String TO_EMIL_SUFFIX = "@konkuk.ac.kr";
+    private static final String VERIFICATION_CODE_SUBJECT = "[쿠링] 이메일 인증 코드 발송";
+
     @Override
     public void sendVerificationEmail(String email) {
-        final String FROM_EMAIL = "no-reply@ku-ring.com";
-        final String TO_EMIL_SUFFIX = "@konkuk.ac.kr";
-        final String VERIFICATION_CODE_SUBJECT = "[쿠링] 이메일 인증 코드 발송";
         if (!email.endsWith(TO_EMIL_SUFFIX)) {
             throw new EmailBusinessException(ErrorCode.EMAIL_INVALID_SUFFIX);
         }
-        String code = createCode();
-        verificationCodeCommandPort.saveCode(email, code);
-        String htmlTextWithCode = createHtmlTextWithCode(code);
-        emailEventPort.sendEmailEvent(FROM_EMAIL, email, VERIFICATION_CODE_SUBJECT, htmlTextWithCode);
+        String code = createVerificationCode();
+        Map<String, Object> variables = createVariables(code);
+        String htmlTextWithCode = templateEnginePort.process(TEMPLATE_FILE, variables);
+        MimeMessage mimeMessage = createMimeMessage(FROM_EMAIL, email, VERIFICATION_CODE_SUBJECT, htmlTextWithCode);
+        emailClientPort.sendEmailAsync(mimeMessage);
+        verificationCodeCommandPort.saveVerificationCode(email, code);
     }
 
-    @Override
-    public void sendEmail(EmailSendCommand command) {
-        emailClientPort.send(command.from(), command.to(), command.subject(), command.text());
+    private MimeMessage createMimeMessage(String from, String to, String subject, String text) {
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(new MimeMessage(Session.getInstance(System.getProperties())),"utf-8");
+        try {
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setTo(to);
+            mimeMessageHelper.setSubject(subject);
+            mimeMessageHelper.setText(text, true);
+        } catch (MessagingException e) {
+            throw new EmailBusinessException(ErrorCode.EMAIL_INVALID_TEMPLATE);
+        }
+        return mimeMessageHelper.getMimeMessage();
     }
 
-    private String createHtmlTextWithCode(String code) {
-        final String TEMPLATE_FILE = "mail/email-verification";
-        Context context = new Context();
-        context.setVariable("code", code);
-        return templateEngine.process(TEMPLATE_FILE, context);
+    private Map<String, Object> createVariables(String code) {
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("code", code);
+        return variables;
     }
 
-    private String createCode() {
+    private String createVerificationCode() {
         final int length = 6;
         try {
             Random random = SecureRandom.getInstanceStrong();
