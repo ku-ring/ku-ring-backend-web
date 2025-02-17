ba1789ef532bd9a8954b24e7da7b7efbd5aab2aa
Han Ju Kim <hjkim2902@gmail.com>
2025-02-17 12:29:09 +0900

    fix : 이메일 검증 테스트 수정

--- src/test/java/com/kustacks/kuring/email/application/service/EmailVerifyUseCaseTest.java
+++ src/test/java/com/kustacks/kuring/email/application/service/EmailVerifyUseCaseTest.java
@@ -2,17 +2,21 @@ package com.kustacks.kuring.email.application.service;
 
 import com.kustacks.kuring.email.application.port.out.VerificationCodeQueryPort;
 import com.kustacks.kuring.email.application.service.exception.EmailBusinessException;
+import com.kustacks.kuring.email.domain.VerificationCode;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class EmailVerifyUseCaseTest {
 
     private EmailVerifyService emailVerifyUseCase;
 
-    @MockBean
+    @Mock
     private VerificationCodeQueryPort verificationCodeQueryPort = Mockito.mock(VerificationCodeQueryPort.class);
 
     public EmailVerifyUseCaseTest() {
@@ -25,7 +29,7 @@ class EmailVerifyUseCaseTest {
         //given
         String email = "test@test.com";
         String code = "123456";
-        Mockito.when(verificationCodeQueryPort.findCodeByEmail(email)).thenReturn(code);
+        Mockito.when(verificationCodeQueryPort.findCodesByEmail(email)).thenReturn(List.of(new VerificationCode(email, code)));
 
         //when, then
         Assertions.assertThatCode(
@@ -38,14 +42,12 @@ class EmailVerifyUseCaseTest {
     void fail_verify_code() {
         //given
         String email = "test@test.com";
-        String code = "123456";
-
         String wrongCode = "654321";
-        Mockito.when(verificationCodeQueryPort.findCodeByEmail(email)).thenReturn(wrongCode);
+        Mockito.when(verificationCodeQueryPort.findCodesByEmail(email)).thenReturn(new ArrayList<>());
 
         //when, then
         Assertions.assertThatThrownBy(
-                        () -> emailVerifyUseCase.verifyCode(email, code))
+                        () -> emailVerifyUseCase.verifyCode(email, wrongCode))
                 .isInstanceOf(EmailBusinessException.class);
     }
 }
