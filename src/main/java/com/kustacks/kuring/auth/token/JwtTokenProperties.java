73053476a751619aa6efed3610ef6929f8e0ef80
Han Ju Kim <hjkim2902@gmail.com>
2025-03-03 05:07:12 +0900

    feat : userExpireLength 추가 및 User Bearer Token 생성 메소드 추가

--- src/main/java/com/kustacks/kuring/auth/token/JwtTokenProperties.java
+++ src/main/java/com/kustacks/kuring/auth/token/JwtTokenProperties.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "security.jwt.token")
 public record JwtTokenProperties(
         String secretKey,
-        long expireLength
+        long expireLength,
+        long userExpireLength
 ) {
 }
