95af5c91ca9a107feac3cbb744653b0b8d70f40d
Han Ju Kim <hjkim2902@gmail.com>
2025-03-03 05:09:19 +0900

    feat : JwtTokenProperties변경에 따른 테스트 수정

--- src/test/java/com/kustacks/kuring/auth/token/JwtTokenProviderTest.java
+++ src/test/java/com/kustacks/kuring/auth/token/JwtTokenProviderTest.java
@@ -16,17 +16,18 @@ class JwtTokenProviderTest {
 
     private String secretKey = "test-secret-key-test-secret-key-test-secret-key-test-secret-key";
     private long expireLength = 3600000;
+    private long userExpireLength = 3600000;
 
     @Test
     @DisplayName("토큰을 성공적으로 발급하고 payload 추출한다")
     void parse_payload_by_valid_token() {
         // given
-        JwtTokenProperties properties = new JwtTokenProperties(secretKey, expireLength);
+        JwtTokenProperties properties = new JwtTokenProperties(secretKey, expireLength, userExpireLength);
         JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(properties);
         String userId = "shine_id";
 
         // when
-        String token = jwtTokenProvider.createToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
+        String token = jwtTokenProvider.createAdminToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
 
         // then
         assertAll(
@@ -39,10 +40,10 @@ class JwtTokenProviderTest {
     @DisplayName("만료된 토큰에서 payload 추출 시 예외를 반환한다")
     void parse_payload_by_expired_token() {
         // given
-        JwtTokenProperties properties = new JwtTokenProperties(secretKey, -1l);
+        JwtTokenProperties properties = new JwtTokenProperties(secretKey, -1l, -1l);
         JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(properties);
         String userId = "shine_id";
-        String expiredToken = jwtTokenProvider.createToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
+        String expiredToken = jwtTokenProvider.createAdminToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
 
         // when
         ThrowingCallable callable = () -> jwtTokenProvider.getPrincipal(expiredToken);
@@ -55,14 +56,14 @@ class JwtTokenProviderTest {
     @DisplayName("잘못된 키로 토큰을 발급할 경우 payload 추출 시 예외를 반환한다")
     void parse_payload_by_invalid_token() {
         // given
-        JwtTokenProperties validProperties = new JwtTokenProperties(secretKey, expireLength);
-        JwtTokenProperties invalidProperties = new JwtTokenProperties("invalid-key", expireLength);
+        JwtTokenProperties validProperties = new JwtTokenProperties(secretKey, expireLength, userExpireLength);
+        JwtTokenProperties invalidProperties = new JwtTokenProperties("invalid-key", expireLength, userExpireLength);
 
         JwtTokenProvider validJwtTokenProvider = new JwtTokenProvider(validProperties);
         JwtTokenProvider invalidJwtTokenProvider = new JwtTokenProvider(invalidProperties);
 
         String userId = "shine_id";
-        String token = validJwtTokenProvider.createToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
+        String token = validJwtTokenProvider.createAdminToken(userId, List.of(AdminRole.ROLE_ROOT.name(), AdminRole.ROLE_CLIENT.name()));
 
         // when
         ThrowingCallable callable = () -> invalidJwtTokenProvider.getPrincipal(token);
