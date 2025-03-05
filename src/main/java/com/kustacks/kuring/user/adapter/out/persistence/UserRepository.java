643d090b4a6c266edf05fd69ba289107e5d32cf4
Han Ju Kim <hjkim2902@gmail.com>
2025-03-05 19:46:24 +0900

    fix : User Repository 변경

--- src/main/java/com/kustacks/kuring/user/adapter/out/persistence/UserRepository.java
+++ src/main/java/com/kustacks/kuring/user/adapter/out/persistence/UserRepository.java
@@ -4,11 +4,12 @@ import com.kustacks.kuring.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 interface UserRepository extends JpaRepository<User, Long>, UserQueryRepository {
-    Optional<User> findByNickname(String nickname);
+    Optional<User> findByFcmToken(String fcmToken);
 
-    @Query("SELECT u FROM User u WHERE u.email = :email")
-    Optional<User> findByEmail(String email);
+    @Query("SELECT u.fcmToken FROM User u")
+    List<String> findAllFcmTokens();
 }
