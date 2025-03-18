5f33f80da19b267833ce86a1f554b93893526923
Han Ju Kim <hjkim2902@gmail.com>
2025-03-18 17:29:26 +0900

    feat : RootUser정보 조회 API 생성

--- src/main/java/com/kustacks/kuring/user/adapter/in/web/UserQueryApiV2.java
+++ src/main/java/com/kustacks/kuring/user/adapter/in/web/UserQueryApiV2.java
@@ -1,18 +1,26 @@
 package com.kustacks.kuring.user.adapter.in.web;
 
+import com.kustacks.kuring.auth.authentication.AuthorizationExtractor;
+import com.kustacks.kuring.auth.authentication.AuthorizationType;
+import com.kustacks.kuring.auth.token.JwtTokenProvider;
 import com.kustacks.kuring.common.annotation.RestWebAdapter;
 import com.kustacks.kuring.common.dto.BaseResponse;
+import com.kustacks.kuring.common.exception.InvalidStateException;
+import com.kustacks.kuring.common.exception.code.ErrorCode;
 import com.kustacks.kuring.user.adapter.in.web.dto.UserAIAskCountResponse;
 import com.kustacks.kuring.user.adapter.in.web.dto.UserBookmarkResponse;
 import com.kustacks.kuring.user.adapter.in.web.dto.UserCategoryNameResponse;
 import com.kustacks.kuring.user.adapter.in.web.dto.UserDepartmentNameResponse;
+import com.kustacks.kuring.user.adapter.in.web.dto.UserInfoResponse;
 import com.kustacks.kuring.user.application.port.in.UserQueryUseCase;
 import com.kustacks.kuring.user.application.port.in.dto.UserAIAskCountResult;
+import com.kustacks.kuring.user.application.port.in.dto.UserInfoResult;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +28,11 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.List;
 
-import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.*;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.ASK_COUNT_LOOKUP_SUCCESS;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.BOOKMARK_LOOKUP_SUCCESS;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.CATEGORY_USER_SUBSCRIBES_LOOKUP_SUCCESS;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.DEPARTMENTS_USER_SUBSCRIBES_LOOKUP_SUCCESS;
+import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.USER_INFO_LOOKUP_SUCCESS;
 
 @Tag(name = "User-Query", description = "사용자가 주체가 되는 정보 조회")
 @Slf4j
@@ -30,8 +42,10 @@ import static com.kustacks.kuring.common.dto.ResponseCodeAndMessages.*;
 class UserQueryApiV2 {
 
     private static final String FCM_TOKEN_HEADER_KEY = "User-Token";
+    private static final String JWT_TOKEN_HEADER_KEY = "JWT";
 
     private final UserQueryUseCase userQueryUseCase;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Operation(summary = "사용자 카테고리 조회", description = "사용자가 구독한 카테고리 목록을 조회합니다")
     @SecurityRequirement(name = FCM_TOKEN_HEADER_KEY)
@@ -84,5 +98,42 @@ class UserQueryApiV2 {
         UserAIAskCountResult result = userQueryUseCase.lookupUserAIAskCount(userToken);
         UserAIAskCountResponse response = UserAIAskCountResponse.from(result);
         return ResponseEntity.ok().body(new BaseResponse<>(ASK_COUNT_LOOKUP_SUCCESS, response));
+    }
+
+    @Operation(summary = "사용자 정보 조회", description = "사용자 정보를 조회합니다.")
+    @SecurityRequirement(name = JWT_TOKEN_HEADER_KEY)
+    @GetMapping("/user-me")
+    public ResponseEntity<BaseResponse<UserInfoResponse>> lookupUserInfo(
+                        @RequestHeader (AuthorizationExtractor.AUTHORIZATION) String bearerToken
+    ) {
+        String jwtToken = extract(bearerToken, AuthorizationType.BEARER);
+        String email = validateJwtAndGetEmail(jwtToken);
+
+        UserInfoResult result = userQueryUseCase.lookupUserInfo(email);
+        UserInfoResponse response = UserInfoResponse.from(result);
+        return ResponseEntity.ok().body(new BaseResponse<>(USER_INFO_LOOKUP_SUCCESS, response));
+    }
+
+    private String validateJwtAndGetEmail(String jwtToken) {
+        if (!jwtTokenProvider.validateToken(jwtToken)) {
+            throw new InvalidStateException(ErrorCode.JWT_INVALID_TOKEN);
+        }
+        return jwtTokenProvider.getPrincipal(jwtToken);
+    }
+
+    private String extract(String value, AuthorizationType type) {
+        String typeToLowerCase = type.toLowerCase();
+        int typeLength = typeToLowerCase.length();
+
+        if ((value.toLowerCase().startsWith(typeToLowerCase))) {
+            String authHeaderValue = value.substring(typeLength).trim();
+            int commaIndex = authHeaderValue.indexOf(',');
+            if (commaIndex > 0) {
+                authHeaderValue = authHeaderValue.substring(0, commaIndex);
+            }
+            return authHeaderValue;
+        }
+
+        return Strings.EMPTY;
     }
 }
