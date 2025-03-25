c56ce1a16344df72f28fd923ffb3820d1102a517
Han Ju Kim <hjkim2902@gmail.com>
2025-03-25 14:21:30 +0900

    feat : 쿠링봇 질문 시 로그인 여부(Jwt)로 질문하도록 EventPort & Adapter 추가

--- src/main/java/com/kustacks/kuring/ai/adapter/out/event/RAGEventAdapter.java
+++ src/main/java/com/kustacks/kuring/ai/adapter/out/event/RAGEventAdapter.java
@@ -1,5 +1,6 @@
 package com.kustacks.kuring.ai.adapter.out.event;
 
+import com.kustacks.kuring.ai.adapter.in.event.dto.RootUserDecreaseQuestionCountEvent;
 import com.kustacks.kuring.ai.application.port.out.RAGEventPort;
 import com.kustacks.kuring.common.domain.Events;
 import com.kustacks.kuring.user.adapter.in.event.dto.UserDecreaseQuestionCountEvent;
@@ -11,5 +12,10 @@ public class RAGEventAdapter implements RAGEventPort {
     @Override
     public void userDecreaseQuestionCountEvent(String userId) {
         Events.raise(new UserDecreaseQuestionCountEvent(userId));
+    }
+
+    @Override
+    public void rootUserDecreaseQuestionCountEvent(String userId, String email) {
+        Events.raise(new RootUserDecreaseQuestionCountEvent(userId, email));
     }
 }
--- src/main/java/com/kustacks/kuring/ai/application/port/out/RAGEventPort.java
+++ src/main/java/com/kustacks/kuring/ai/application/port/out/RAGEventPort.java
@@ -2,4 +2,6 @@ package com.kustacks.kuring.ai.application.port.out;
 
 public interface RAGEventPort {
     void userDecreaseQuestionCountEvent(String userId);
+
+    void rootUserDecreaseQuestionCountEvent(String userId, String email);
 }
