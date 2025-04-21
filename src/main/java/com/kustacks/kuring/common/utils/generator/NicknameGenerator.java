0c446b80c8123163ed854388ee9db76d32b6d5ea
Han Ju Kim <hjkim2902@gmail.com>
2025-04-21 20:51:33 +0900

    feat : NicknameGenerator 추가

---
+++ src/main/java/com/kustacks/kuring/common/utils/generator/NicknameGenerator.java
@@ -0,0 +1,19 @@
+package com.kustacks.kuring.common.utils.generator;
+
+public class NicknameGenerator {
+    private static final int NICKNAME_NUMBER_LENGTH = 6;
+    private static final String[] NICKNAME_PREFIXES = {"쿠링이", "건덕이", "건구스"};
+
+    public static String generateNickname() {
+        // 접두사(쿠링이, 건덕이, 건구스 중 하나) + 6자리 숫자
+        String prefix = pickRandomNicknamePrefix();
+        String suffix = RandomGenerator.generateRandomNumber(NICKNAME_NUMBER_LENGTH);
+
+        return prefix + suffix;
+    }
+
+    private static String pickRandomNicknamePrefix() {
+        int index = RandomGenerator.generatePositiveNumber(NICKNAME_PREFIXES.length);
+        return NICKNAME_PREFIXES[index];
+    }
+}
