981ab2d57c3f870571f5c74ea79526a08b277c95
Han Ju Kim <hjkim2902@gmail.com>
2025-06-02 10:05:37 +0900

    feat: ReportPersistenceAdapter 추가

---
+++ src/main/java/com/kustacks/kuring/report/adapter/out/persistence/ReportPersistenceAdapter.java
@@ -0,0 +1,36 @@
+package com.kustacks.kuring.report.adapter.out.persistence;
+
+import com.kustacks.kuring.admin.application.port.out.AdminUserReportPort;
+import com.kustacks.kuring.common.annotation.PersistenceAdapter;
+import com.kustacks.kuring.report.application.port.out.ReportCommandPort;
+import com.kustacks.kuring.report.application.port.out.ReportQueryPort;
+import com.kustacks.kuring.report.domain.Report;
+import com.kustacks.kuring.report.domain.ReportTargetType;
+import com.kustacks.kuring.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+@PersistenceAdapter
+@RequiredArgsConstructor
+class ReportPersistenceAdapter implements ReportCommandPort, ReportQueryPort, AdminUserReportPort {
+
+    private final ReportRepository reportRepository;
+
+    @Override
+    public void save(Report report) {
+        reportRepository.save(report);
+    }
+
+    @Override
+    public Optional<Report> findByReporterAndTargetIdAndType(User user, Long targetId, ReportTargetType type) {
+        return reportRepository.findByReporterAndTargetIdAndTargetType(user, targetId, type);
+    }
+
+    @Override
+    public Page<Report> findAllReportByPageRequest(Pageable pageable) {
+        return reportRepository.findAll(pageable);
+    }
+}
