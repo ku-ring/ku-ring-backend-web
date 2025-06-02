4c83610c748a5270b5a7fec2fe44d3afdc9e0b61
Han Ju Kim <hjkim2902@gmail.com>
2025-06-02 10:09:33 +0900

    test: report 테이블 DDL 추가

---
+++ src/main/resources/db/migration/V250530__Create_Report_Table.sql
@@ -0,0 +1,38 @@
+CREATE TABLE IF NOT EXISTS `report`
+(
+    id
+    BIGINT
+    NOT
+    NULL
+    AUTO_INCREMENT,
+    target_id
+    BIGINT
+    NOT
+    NULL,
+    target_type
+    VARCHAR
+(
+    30
+) NOT NULL,
+    content VARCHAR
+(
+    256
+) NOT NULL,
+    reporter_id BIGINT,
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    PRIMARY KEY
+(
+    id
+),
+    CONSTRAINT FK_reportTBL_userTBL
+    FOREIGN KEY
+(
+    reporter_id
+) REFERENCES user
+(
+    id
+) ON DELETE SET NULL
+    ) ENGINE = InnoDB
+    DEFAULT CHARSET = utf8mb4
+    COLLATE = utf8mb4_unicode_ci;
