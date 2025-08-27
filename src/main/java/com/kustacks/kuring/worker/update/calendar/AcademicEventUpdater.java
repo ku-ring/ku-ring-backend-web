179b42c4f0dd41c1fad8e579cfe35d73db2ab075
Han Ju Kim <hjkim2902@gmail.com>
2025-08-27 19:19:48 +0900

    feat: AcademicEventUpdater 구현

---
+++ src/main/java/com/kustacks/kuring/worker/update/calendar/AcademicEventUpdater.java
@@ -0,0 +1,42 @@
+package com.kustacks.kuring.worker.update.calendar;
+
+import com.kustacks.kuring.worker.parser.calendar.IcsParser;
+import com.kustacks.kuring.worker.parser.calendar.dto.IcsCalendarResult;
+import com.kustacks.kuring.worker.scrap.calendar.IcsScraper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.fortuna.ical4j.data.ParserException;
+import net.fortuna.ical4j.model.Calendar;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AcademicEventUpdater {
+
+    private final IcsScraper icsScraper;
+    private final IcsParser icsParser;
+    private final AcademicEventDbSynchronizer academicEventDbSynchronizer;
+
+    @Value("${calendar.academic-ics-url}")
+    private String calendarUrl;
+
+    @Scheduled(cron = "0 5/2 * * * *", zone = "Asia/Seoul")
+    public void update() {
+        log.info("******** 학사일정 업데이트 시작 ********");
+        try {
+            Calendar iCalendar = icsScraper.scrapICalendar(calendarUrl);
+            IcsCalendarResult result = icsParser.parse(iCalendar);
+            academicEventDbSynchronizer.compareAndUpdateDb(result);
+        } catch (IOException e) {
+            log.error("학교 Outlook 캘린더 연결에 실패하였습니다.");
+        } catch (ParserException e) {
+            log.error("ICS 파싱에 실패하였습니다.");
+        }
+        log.info("******** 학사일정 업데이트 완료 ********");
+    }
+}
