335a2e52b5ffe8b11c7137c4456d459d054a53af
Han Ju Kim <hjkim2902@gmail.com>
2025-08-27 19:20:06 +0900

    feat: Transparent 생성 메서드 구현

--- src/main/java/com/kustacks/kuring/calendar/domain/Transparent.java
+++ src/main/java/com/kustacks/kuring/calendar/domain/Transparent.java
@@ -4,4 +4,14 @@ public enum Transparent {
     OPAQUE, //바쁨(중요일정)
     TRANSPARENT //바쁘지 않음(미중요일정)
     ;
+
+    public static Transparent valueOfString(String transp) {
+        transp = transp.trim().toUpperCase();
+
+        return switch (transp) {
+            case "TRANSPARENT" -> TRANSPARENT;
+            case "OPAQUE" -> OPAQUE;
+            default -> null;
+        };
+    }
 }
