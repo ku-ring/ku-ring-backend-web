6b1f53f76219eb43fec9d2884bd0a2f87c4cec9b
jiwoo <zbqmgldjfh@gmail.com>
2025-08-31 11:41:45 +0900

    feat(UserUpdater): FeatureFlag 목으로 설정

--- src/test/java/com/kustacks/kuring/worker/update/user/UserUpdaterTest.java
+++ src/test/java/com/kustacks/kuring/worker/update/user/UserUpdaterTest.java
@@ -1,5 +1,6 @@
 package com.kustacks.kuring.worker.update.user;
 
+import com.kustacks.kuring.common.featureflag.FeatureFlags;
 import com.kustacks.kuring.support.IntegrationTestSupport;
 import com.kustacks.kuring.user.adapter.out.persistence.UserPersistenceAdapter;
 import com.kustacks.kuring.user.domain.RootUser;
@@ -8,9 +9,12 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 class UserUpdaterTest extends IntegrationTestSupport {
 
@@ -23,11 +27,16 @@ class UserUpdaterTest extends IntegrationTestSupport {
     @Autowired
     UserUpdater userUpdater;
 
+    @MockBean
+    private FeatureFlags featureFlags;
+
     @Transactional // em.flush(), clear()를 위해 설정
     @DisplayName("사용자 질문 카운트가 감소해도 매월 초에 초기값으로 다시 설정된다")
     @Test
     void questionCountReset() {
         // given
+        when(featureFlags.isEnabled(any())).thenReturn(true);
+
         User savedUser = userPersistenceAdapter.findByToken(USER_FCM_TOKEN).get();
         savedUser.decreaseQuestionCount(); // 2 -> 1
         savedUser.decreaseQuestionCount(); // 1 -> 0
@@ -48,10 +57,27 @@ class UserUpdaterTest extends IntegrationTestSupport {
     @Test
     void rootUserQuestionCountReset() {
         // given
+        when(featureFlags.isEnabled(any())).thenReturn(true);
+
         RootUser savedRootUser = userPersistenceAdapter.findRootUserByEmail(USER_EMAIL).get();
         savedRootUser.updateQuestionCount(0);
         em.flush();
         em.clear();
+
+        // when
+        userUpdater.questionCountReset(); // 질문 카운트 리셋 메서드 호출
+
+        // then
+        RootUser findRootUser = userPersistenceAdapter.findRootUserByEmail(USER_EMAIL).get();
+        assertThat(findRootUser.getQuestionCount()).isEqualTo(RootUser.ROOT_USER_MONTHLY_QUESTION_COUNT);
+    }
+
+    @Transactional
+    @DisplayName("피쳐플레그가 false면 사용자 질문 카운트 리셋이 실행되지 않는다")
+    @Test
+    void rootUserQuestionCountCantReset() {
+        // given
+        when(featureFlags.isEnabled(any())).thenReturn(false);
 
         // when
         userUpdater.questionCountReset(); // 질문 카운트 리셋 메서드 호출
