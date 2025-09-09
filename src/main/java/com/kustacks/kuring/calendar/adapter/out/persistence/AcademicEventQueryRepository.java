2f23efca3f0843019c2a62f6cf55f21dcf3c9745
Han Ju Kim <hjkim2902@gmail.com>
2025-09-09 23:08:33 +0900

    feat: 학사일정 조회 AcademicEventQueryRepository & Impl 구현

--- src/main/java/com/kustacks/kuring/calendar/adapter/out/persistence/AcademicEventQueryRepository.java
+++ src/main/java/com/kustacks/kuring/calendar/adapter/out/persistence/AcademicEventQueryRepository.java
@@ -1,10 +1,20 @@
 package com.kustacks.kuring.calendar.adapter.out.persistence;
 
+import com.kustacks.kuring.calendar.application.port.out.dto.AcademicEventReadModel;
 import com.kustacks.kuring.calendar.domain.AcademicEvent;
 
+import java.time.LocalDate;
 import java.util.List;
 
 interface AcademicEventQueryRepository {
 
-    List<AcademicEvent> findAllByEventUids(List<String> eventUids);
+    List<AcademicEvent> findByEventUids(List<String> eventUids);
+
+    List<AcademicEventReadModel> findAllEventReadModels();
+
+    List<AcademicEventReadModel> findEventsAfter(LocalDate startDate);
+
+    List<AcademicEventReadModel> findEventsBefore(LocalDate endDate);
+
+    List<AcademicEventReadModel> findEventsBetweenDate(LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/kustacks/kuring/calendar/adapter/out/persistence/AcademicEventQueryRepositoryImpl.java
+++ src/main/java/com/kustacks/kuring/calendar/adapter/out/persistence/AcademicEventQueryRepositoryImpl.java
@@ -1,9 +1,14 @@
 package com.kustacks.kuring.calendar.adapter.out.persistence;
 
+import com.kustacks.kuring.calendar.application.port.out.dto.AcademicEventReadModel;
+import com.kustacks.kuring.calendar.application.port.out.dto.QAcademicEventReadModel;
 import com.kustacks.kuring.calendar.domain.AcademicEvent;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.kustacks.kuring.calendar.domain.QAcademicEvent.academicEvent;
@@ -14,11 +19,67 @@ class AcademicEventQueryRepositoryImpl implements AcademicEventQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<AcademicEvent> findAllByEventUids(List<String> eventUids) {
+    public List<AcademicEvent> findByEventUids(List<String> eventUids) {
         return queryFactory
                 .select(academicEvent)
                 .from(academicEvent)
                 .where(academicEvent.eventUid.in(eventUids))
                 .fetch();
+    }
+
+    @Override
+    public List<AcademicEventReadModel> findAllEventReadModels() {
+        return queryFactorySelectingReadModel()
+                .fetch();
+    }
+
+    @Override
+    public List<AcademicEventReadModel> findEventsAfter(LocalDate startDate) {
+        return queryFactorySelectingReadModel()
+                .where(buildBooleanExpressionByDates(startDate, null))
+                .fetch();
+    }
+
+
+    @Override
+    public List<AcademicEventReadModel> findEventsBefore(LocalDate endDate) {
+        return queryFactorySelectingReadModel()
+                .where(buildBooleanExpressionByDates(null, endDate))
+                .fetch();
+    }
+
+    @Override
+    public List<AcademicEventReadModel> findEventsBetweenDate(LocalDate startDate, LocalDate endDate) {
+        return queryFactorySelectingReadModel()
+                .where(buildBooleanExpressionByDates(startDate, endDate))
+                .fetch();
+    }
+
+    private JPAQuery<AcademicEventReadModel> queryFactorySelectingReadModel() {
+        return queryFactory
+                .select(new QAcademicEventReadModel(
+                        academicEvent.id,
+                        academicEvent.eventUid,
+                        academicEvent.summary,
+                        academicEvent.description,
+                        academicEvent.category,
+                        academicEvent.transparent,
+                        academicEvent.sequence,
+                        academicEvent.notifyEnabled,
+                        academicEvent.startTime,
+                        academicEvent.endTime
+                ))
+                .from(academicEvent);
+    }
+
+    private BooleanExpression buildBooleanExpressionByDates(LocalDate startDate, LocalDate endDate) {
+        if (startDate == null) {
+            return academicEvent.startTime.lt(endDate.plusDays(1L).atStartOfDay());
+        }
+        if (endDate == null) {
+            return academicEvent.startTime.goe(startDate.atStartOfDay());
+        }
+        return academicEvent.startTime.lt(endDate.plusDays(1L).atStartOfDay())
+                .and(academicEvent.endTime.goe(startDate.atStartOfDay()));
     }
 }
