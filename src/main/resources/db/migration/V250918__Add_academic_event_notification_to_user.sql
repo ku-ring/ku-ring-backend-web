f9f5b9e23b90771f18adc34c0ad180c18724cf91
Han Ju Kim <hjkim2902@gmail.com>
2025-09-20 15:29:21 +0900

    fix: 학사일정 알림 ON/OFF 컬럼 추가 시 불필요 쿼리문 제거

--- src/main/resources/db/migration/V250918__Add_academic_event_notification_to_user.sql
+++ src/main/resources/db/migration/V250918__Add_academic_event_notification_to_user.sql
@@ -1,8 +1,3 @@
 -- 사용자 테이블에 학사일정 알림 설정 컬럼 추가
 ALTER TABLE user
     ADD COLUMN academic_event_notification_enabled BOOLEAN NOT NULL DEFAULT TRUE;
-
--- 기존 데이터에 대해서도 명시적으로 true 설정
-UPDATE user
-SET academic_event_notification_enabled = TRUE
-WHERE academic_event_notification_enabled IS NULL;
