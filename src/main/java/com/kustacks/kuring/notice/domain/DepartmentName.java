0584b9f203042773f415007b2c8c605cb2baff7b
양지윤 <155087951+jiyun921@users.noreply.github.com>
2026-01-24 22:34:12 +0900

    [Fix] 부동산학과 공지 조회 hostPrefix 호환 (#332)
    * [refactor]: hostprefix - rest도 부동산학과 조회 가능하도록 수정
    
    * [refactor]: 학과 공지 조회 조건 null 방어 처리
    
    * [test]: DepartmentName fromHostPrefix (rest/kure) 변환 및 예외 테스트 추가
    
    * [refactor]: DepartmentName legacy hostPrefix 분리
    
    * [test]: hostPrefixMap 테스트 구현
    
    * [refactor]: 변수명 fallbackHostPrefix로 변경
    
    * [refactor]: 변수명 fallbackHostPrefix로 변경
    
    * [refactor]: hostPrefix 매칭 책임 캡슐화
    
    * [refactor]: HOST_PREFIX_MAP O(n)으로 로직 수정

--- src/main/java/com/kustacks/kuring/notice/domain/DepartmentName.java
+++ src/main/java/com/kustacks/kuring/notice/domain/DepartmentName.java
@@ -6,93 +6,95 @@ import lombok.Getter;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.kustacks.kuring.common.exception.code.ErrorCode.DEPARTMENT_NOT_FOUND;
 
 @Getter
 public enum DepartmentName {
 
-    KOREAN("korean", "korea", "국어국문학과"),
-    ENGLISH("english", "english", "영어영문학과"),
-    CHINESE("chinese", "china", "중어중문학과"),
-    PHILOSOPHY("philosophy", "philo", "철학과"),
-    HISTORY("khistory", "khistory", "사학과"),
-    GEOLOGY("geology", "kugeo", "지리학과"),
-    MEDIA_COMM("media_communication", "comm", "미디어커뮤니케이션학과"),
-    CULTURE_CONT("culture_content", "culturecontents", "문화콘텐츠학과"),
+    KOREAN("korean", "korea", null, "국어국문학과"),
+    ENGLISH("english", "english", null, "영어영문학과"),
+    CHINESE("chinese", "china", null, "중어중문학과"),
+    PHILOSOPHY("philosophy", "philo", null, "철학과"),
+    HISTORY("khistory", "khistory", null, "사학과"),
+    GEOLOGY("geology", "kugeo", null, "지리학과"),
+    MEDIA_COMM("media_communication", "comm", null, "미디어커뮤니케이션학과"),
+    CULTURE_CONT("culture_content", "culturecontents", null, "문화콘텐츠학과"),
 
-    MATH("math", "math", "수학과"),
-    PHYSICS("physics", "phys", "물리학과"),
-    CHEMICALS("chemicals", "chemi", "화학과"),
+    MATH("math", "math", null, "수학과"),
+    PHYSICS("physics", "phys", null, "물리학과"),
+    CHEMICALS("chemicals", "chemi", null, "화학과"),
 
-    ARCHITECTURE("architecture", "caku", "건축학부"),
+    ARCHITECTURE("architecture", "caku", null, "건축학부"),
 
-    CIVIL_ENV("civil_environment", "cee", "사회환경공학부"),
-    MECH_AERO("mechanical_aerospace", "mae", "기계항공공학부"),
-    ELEC_ELEC("electrical_electronics", "ee", "전자전기공학부"),
-    CHEMI_DIV("chemical_division", "chemeng", "화학공학부"),
-    COMPUTER("computer_science", "cse", "컴퓨터공학부"),
-    INDUSTRIAL("industrial", "kies", "산업공학과"),
-    ADV_INDUSTRIAL("advanced_industrial", "aif", "신산업융합학과"),
-    BIOLOGICAL("biological", "microbio", "생물공학과"),
-    KBEAUTY("kbeauty_industry_fusion", "kbeauty", "K뷰티산업융합학과"),
-    MECHA_ROBOT_AUTO("mechanical_robot_automotive", "me", "기계로봇자동차공학부"),
-    MATERIAL("materials_science", "mse", "재료공학과"),
-    AERO_MOBILITY("aerospace_mobility", "aeroeng", "항공우주모빌리티공학과"),
+    CIVIL_ENV("civil_environment", "cee", null, "사회환경공학부"),
+    MECH_AERO("mechanical_aerospace", "mae", null, "기계항공공학부"),
+    ELEC_ELEC("electrical_electronics", "ee", null, "전자전기공학부"),
+    CHEMI_DIV("chemical_division", "chemeng", null, "화학공학부"),
+    COMPUTER("computer_science", "cse", null, "컴퓨터공학부"),
+    INDUSTRIAL("industrial", "kies", null, "산업공학과"),
+    ADV_INDUSTRIAL("advanced_industrial", "aif", null, "신산업융합학과"),
+    BIOLOGICAL("biological", "microbio", null, "생물공학과"),
+    KBEAUTY("kbeauty_industry_fusion", "kbeauty", null, "K뷰티산업융합학과"),
+    MECHA_ROBOT_AUTO("mechanical_robot_automotive", "me", null, "기계로봇자동차공학부"),
+    MATERIAL("materials_science", "mse", null, "재료공학과"),
+    AERO_MOBILITY("aerospace_mobility", "aeroeng", null, "항공우주모빌리티공학과"),
 
-    POLITICS("political_science", "kupol", "정치외교학과"),
-    ECONOMICS("economics", "econ", "경제학과"),
-    ADMINISTRATION("public_administration", "kkupa", "행정학과"),
-    INT_TRADE("international_trade", "itrade", "국제무역학과"),
-    STATISTICS("applied_statistics", "stat", "응용통계학과"),
-    DISCI_STUDIES("inter_disciplinary_studies", "dola", "융합인재학과"),
-    GLOBAL_BUSI("global_business", "dois", "글로벌비즈니스학과"),
+    POLITICS("political_science", "kupol", null, "정치외교학과"),
+    ECONOMICS("economics", "econ", null, "경제학과"),
+    ADMINISTRATION("public_administration", "kkupa", null, "행정학과"),
+    INT_TRADE("international_trade", "itrade", null, "국제무역학과"),
+    STATISTICS("applied_statistics", "stat", null, "응용통계학과"),
+    DISCI_STUDIES("inter_disciplinary_studies", "dola", null, "융합인재학과"),
+    GLOBAL_BUSI("global_business", "dois", null, "글로벌비즈니스학과"),
 
-    BUIS_ADMIN("business_administration", "biz", "경영학과"),
-    TECH_BUSI("technological_business", "mot", "기술경영학과"),
+    BUIS_ADMIN("business_administration", "biz", null, "경영학과"),
+    TECH_BUSI("technological_business", "mot", null, "기술경영학과"),
 
-    REAL_ESTATE("real_estate", "kure", "부동산학과"),
+    REAL_ESTATE("real_estate", "kure", "rest", "부동산학과"),
 
-    ENERGY("energy", "energy", "미래에너지공학과"),
-    SMART_VEHICLE("smart_vehicle", "smartvehicle", "스마트운행체공학과"),
-    SMART_ICT("smart_ict_convergence", "sicte", "스마트ICT융합공학과"),
-    COSMETICS("cosmetics", "cosmetics", "화장품공학과"),
-    STEM_REGEN("stem_cell_regeneration_bio_technology", "scrb", "줄기세포재생공학과"),
-    BIO_MEDICAL("bio_medical_science", "bmse", "의생명공학과"),
-    SYSTEM_BIO_TECH("system_bio_technology", "kusysbt", "시스템생명공학과"),
-    INT_BIO_TECH("intergrative_bio_technology", "ibb", "융합생명공학과"),
+    ENERGY("energy", "energy", null, "미래에너지공학과"),
+    SMART_VEHICLE("smart_vehicle", "smartvehicle", null, "스마트운행체공학과"),
+    SMART_ICT("smart_ict_convergence", "sicte", null, "스마트ICT융합공학과"),
+    COSMETICS("cosmetics", "cosmetics", null, "화장품공학과"),
+    STEM_REGEN("stem_cell_regeneration_bio_technology", "scrb", null, "줄기세포재생공학과"),
+    BIO_MEDICAL("bio_medical_science", "bmse", null, "의생명공학과"),
+    SYSTEM_BIO_TECH("system_bio_technology", "kusysbt", null, "시스템생명공학과"),
+    INT_BIO_TECH("intergrative_bio_technology", "ibb", null, "융합생명공학과"),
 
-    BIO_SCIENCE("biological_science", "biology", "생명과학특성학과"),
-    ANIMAL_SCIENCE("animal_science_technology", "anis", "동물자원과학과"),
-    CROP_SCIENCE("crop_science", "cropscience", "식량자원과학과"),
-    ANIMAL_RESOURCES("animal_resources_food_science_bio_technology", "foodbio", "축산식품생명공학과"),
-    FOOD_MARKETING("food_marketing_safety", "kufsm", "식품유통공학과"),
-    ENV_HEALTH_SCIENCE("environment_health_science", "ehs", "환경보건과학과"),
-    FORESTRY_LANDSCAPE_ARCH("forestry_landscape_architecture", "fla", "산림조경학과"),
+    BIO_SCIENCE("biological_science", "biology", null, "생명과학특성학과"),
+    ANIMAL_SCIENCE("animal_science_technology", "anis", null, "동물자원과학과"),
+    CROP_SCIENCE("crop_science", "cropscience", null, "식량자원과학과"),
+    ANIMAL_RESOURCES("animal_resources_food_science_bio_technology", "foodbio", null, "축산식품생명공학과"),
+    FOOD_MARKETING("food_marketing_safety", "kufsm", null, "식품유통공학과"),
+    ENV_HEALTH_SCIENCE("environment_health_science", "ehs", null, "환경보건과학과"),
+    FORESTRY_LANDSCAPE_ARCH("forestry_landscape_architecture", "fla", null, "산림조경학과"),
 
-    VET_MEDICINE("veterinary_medicine", "vet", "수의학과"),
+    VET_MEDICINE("veterinary_medicine", "vet", null, "수의학과"),
 
-    COMM_DESIGN("communication_design", "kucd", "커뮤니케이션디자인학과"),
-    IND_DESIGN("industrial_design", "designid", "산업디자인학과"),
-    APPAREL_DESIGN("apparel_design", "apparel", "의상디자인학과"),
-    LIVING_DESIGN("living_design", "livingdesign", "리빙디자인학과"),
-    CONT_ART("contemporary_art", "contemporaryart", "현대미술학과"),
-    MOV_IMAGE("moving_image_film", "movingimages", "영상학과"),
-    MEDIA_ACTING("media_acting", "mediaacting", "매체연기학과"),
+    COMM_DESIGN("communication_design", "kucd", null, "커뮤니케이션디자인학과"),
+    IND_DESIGN("industrial_design", "designid", null, "산업디자인학과"),
+    APPAREL_DESIGN("apparel_design", "apparel", null, "의상디자인학과"),
+    LIVING_DESIGN("living_design", "livingdesign", null, "리빙디자인학과"),
+    CONT_ART("contemporary_art", "contemporaryart", null, "현대미술학과"),
+    MOV_IMAGE("moving_image_film", "movingimages", null, "영상학과"),
+    MEDIA_ACTING("media_acting", "mediaacting", null, "매체연기학과"),
 
-    JAPANESE_EDU("japanese_education", "japan", "일어교육과"),
-    MATH_EDU("mathematics_education", "mathedu", "수학교육과"),
-    PHY_EDU("physical_education", "kupe", "체육교육과"),
-    MUSIC_EDU("music_education", "music", "음악교육과"),
-    EDU_TECH("education_technology", "edutech", "교육공학과"),
-    ENGLISH_EDU("english_education", "englishedu", "영어교육과"),
-    EDUCATION("education", "edu", "교직과"),
+    JAPANESE_EDU("japanese_education", "japan", null, "일어교육과"),
+    MATH_EDU("mathematics_education", "mathedu", null, "수학교육과"),
+    PHY_EDU("physical_education", "kupe", null, "체육교육과"),
+    MUSIC_EDU("music_education", "music", null, "음악교육과"),
+    EDU_TECH("education_technology", "edutech", null, "교육공학과"),
+    ENGLISH_EDU("english_education", "englishedu", null, "영어교육과"),
+    EDUCATION("education", "edu", null, "교직과"),
 
-    ELE_EDU_CENTER("elective_education_center", "sgedu", "교양교육센터"),
-    VOLUNTEER("volunteer_center", "kuvolunteer", "사회봉사센터"),
-    LIBERAL_STUDIES("liberal_studies", "kusls", "KU자유전공학부"),
+    ELE_EDU_CENTER("elective_education_center", "sgedu", null, "교양교육센터"),
+    VOLUNTEER("volunteer_center", "kuvolunteer", null, "사회봉사센터"),
+    LIBERAL_STUDIES("liberal_studies", "kusls", null, "KU자유전공학부"),
 
     ;
     private static final Map<String, String> NAME_MAP;
@@ -103,8 +105,17 @@ public enum DepartmentName {
         NAME_MAP = Collections.unmodifiableMap(Arrays.stream(DepartmentName.values())
                 .collect(Collectors.toMap(DepartmentName::getName, DepartmentName::name)));
 
-        HOST_PREFIX_MAP = Collections.unmodifiableMap(Arrays.stream(DepartmentName.values())
-                .collect(Collectors.toMap(DepartmentName::getHostPrefix, DepartmentName::name)));
+        HOST_PREFIX_MAP = Collections.unmodifiableMap(
+                Arrays.stream(DepartmentName.values())
+                        .flatMap(d -> Stream.of(d.hostPrefix, d.fallbackHostPrefix)
+                                .filter(Objects::nonNull)
+                                .map(prefix -> Map.entry(prefix, d.name()))
+                        )
+                        .collect(Collectors.toMap(
+                                Map.Entry::getKey, Map.Entry::getValue,
+                                (a, b) -> a
+                        ))
+        );
 
         KOR_NAME_MAP = Collections.unmodifiableMap(Arrays.stream(DepartmentName.values())
                 .collect(Collectors.toMap(DepartmentName::getKorName, DepartmentName::name)));
@@ -112,11 +123,13 @@ public enum DepartmentName {
 
     private final String name;
     private final String hostPrefix;
+    private final String fallbackHostPrefix;
     private final String korName;
 
-    DepartmentName(String name, String hostPrefix, String korName) {
+    DepartmentName(String name, String hostPrefix, String fallbackHostPrefix, String korName) {
         this.name = name;
         this.hostPrefix = hostPrefix;
+        this.fallbackHostPrefix = fallbackHostPrefix;
         this.korName = korName;
     }
 
