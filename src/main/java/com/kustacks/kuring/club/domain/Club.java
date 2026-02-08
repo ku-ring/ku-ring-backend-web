60e9601b6a392b037235d9dd418d670da39c763e
jiyun921 <ryn0921@naver.com>
2026-02-08 19:37:07 +0900

    [feat]: Club, ClubBookmark 엔티티 구현

---
+++ src/main/java/com/kustacks/kuring/club/domain/Club.java
@@ -0,0 +1,71 @@
+package com.kustacks.kuring.club.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "club")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Club {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Column(length = 30, nullable = false)
+    private String summary;
+
+    @Lob
+    private String description;
+
+    @Column(length = 30, nullable = false)
+    private String category;
+
+    @Column(length = 30, nullable = false)
+    private String division;
+
+    @Column(name = "instagram_url", length = 255)
+    private String instagramUrl;
+
+    @Column(name = "poster_image_path", length = 255)
+    private String posterImagePath;
+
+    @Column(length = 30)
+    private String building;
+
+    @Column(length = 30)
+    private String room;
+
+    private Double x;
+
+    private Double y;
+
+    @Column(name = "recruit_start_at")
+    private LocalDateTime recruitStartAt;
+
+    @Column(name = "recruit_end_at")
+    private LocalDateTime recruitEndAt;
+
+    @Column(name = "is_always", nullable = false)
+    private boolean isAlways = false;
+
+    @Column(name = "apply_url", length = 255)
+    private String applyUrl;
+
+    @Lob
+    private String qualifications;
+}
