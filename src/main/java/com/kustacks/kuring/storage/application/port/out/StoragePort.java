137b6811589bb0025a0d1fb9081e7c43ae83566e
Han Ju Kim <hjkim2902@gmail.com>
2026-02-12 10:49:30 +0900

    fix : 불필요 throws 구문 수정

--- src/main/java/com/kustacks/kuring/storage/application/port/out/StoragePort.java
+++ src/main/java/com/kustacks/kuring/storage/application/port/out/StoragePort.java
@@ -1,9 +1,9 @@
 package com.kustacks.kuring.storage.application.port.out;
 
-import java.io.*;
+import java.io.InputStream;
 
 public interface StoragePort {
-    void upload(InputStream inputStream, String key, String contentType) throws IOException;
+    void upload(InputStream inputStream, String key, String contentType);
     String getPresignedUrl(String key);
     void delete(String key);
 }
