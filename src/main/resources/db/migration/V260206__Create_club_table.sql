052b98da3fcb7a5de160e42fe9901754675fd8c4
jiyun921 <ryn0921@naver.com>
2026-02-13 13:24:13 +0900

    [refactor]: db club 테이블 변경사항 수정

--- src/main/resources/db/migration/V260206__Create_club_table.sql
+++ src/main/resources/db/migration/V260206__Create_club_table.sql
@@ -6,12 +6,11 @@ CREATE TABLE club
     description       TEXT,
     category          VARCHAR(30) NOT NULL,
     division          VARCHAR(30) NOT NULL,
-    instagram_url     VARCHAR(255),
     poster_image_path VARCHAR(255),
     building          VARCHAR(30),
     room              VARCHAR(30),
-    x DOUBLE,
-    y DOUBLE,
+    lat DOUBLE,
+    lon DOUBLE,
     recruit_start_at  DATETIME,
     recruit_end_at    DATETIME,
     is_always         BOOLEAN     NOT NULL DEFAULT false,
@@ -21,21 +20,39 @@ CREATE TABLE club
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
 
-CREATE TABLE club_bookmark
+CREATE TABLE club_subscribe
 (
     id      BIGINT AUTO_INCREMENT PRIMARY KEY,
     club_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
 
-    CONSTRAINT fk_club_bookmark_club
+    CONSTRAINT fk_club_subscribe_club
         FOREIGN KEY (club_id) REFERENCES club (id)
             ON DELETE CASCADE,
-    CONSTRAINT fk_club_bookmark_user
+    CONSTRAINT fk_club_subscribe_user
         FOREIGN KEY (user_id) REFERENCES user (id)
             ON DELETE CASCADE,
 
-    CONSTRAINT uk_club_user UNIQUE (club_id, user_id)
+    CONSTRAINT uk_club_user UNIQUE (club_id, user_id),
+
+    INDEX idx_club_subscribe_user (user_id),
+    INDEX idx_club_subscribe_club (club_id)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
 
+CREATE TABLE club_sns
+(
+    id      BIGINT AUTO_INCREMENT PRIMARY KEY,
+    club_id BIGINT       NOT NULL,
+    type    VARCHAR(30)  NOT NULL,
+    url     VARCHAR(255) NOT NULL,
+
+    CONSTRAINT fk_club_sns_club
+        FOREIGN KEY (club_id) REFERENCES club (id)
+            ON DELETE CASCADE,
+
+    INDEX   idx_club_sns_club (club_id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
