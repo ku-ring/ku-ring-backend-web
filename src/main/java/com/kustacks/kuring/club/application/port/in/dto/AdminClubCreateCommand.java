0723951a4020caee07188094bcfaf134c8f68caa
Han Ju Kim <hjkim2902@gmail.com>
2026-02-27 16:17:18 +0900

    feat : 동아리 생성 커맨드 정의 추가

---
+++ src/main/java/com/kustacks/kuring/club/application/port/in/dto/AdminClubCreateCommand.java
@@ -0,0 +1,26 @@
+package com.kustacks.kuring.club.application.port.in.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public record AdminClubCreateCommand(
+        String name,
+        String summary,
+        String description,
+        String category,
+        String division,
+        Boolean isAlways,
+        String recruitStartAt,
+        String recruitEndAt,
+        String applyUrl,
+        String qualifications,
+        String instagramUrl,
+        String youtubeUrl,
+        String etcUrl,
+        String building,
+        String room,
+        Double lat,
+        Double lon,
+        MultipartFile iconImage,
+        MultipartFile posterImage
+) {
+}
