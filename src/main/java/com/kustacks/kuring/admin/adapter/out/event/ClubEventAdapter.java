de5ca509d14ba9b5d2c7c2bb043a01b86a32b7b6
Han Ju Kim <hjkim2902@gmail.com>
2026-02-27 16:32:55 +0900

    fix : posterImage Null 분기 처리

--- src/main/java/com/kustacks/kuring/admin/adapter/out/event/ClubEventAdapter.java
+++ src/main/java/com/kustacks/kuring/admin/adapter/out/event/ClubEventAdapter.java
@@ -5,11 +5,11 @@ import com.kustacks.kuring.common.domain.Events;
 import com.kustacks.kuring.common.exception.InvalidStateException;
 import com.kustacks.kuring.common.exception.code.ErrorCode;
 import com.kustacks.kuring.storage.adapter.in.event.dto.ClubCreateEvent;
+import com.kustacks.kuring.storage.adapter.in.event.dto.ClubCreateEvent.ClubCreateImage;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Objects;
 
 @Component
 public class ClubEventAdapter implements ClubEventPort {
@@ -17,16 +17,15 @@ public class ClubEventAdapter implements ClubEventPort {
     @Override
     public void publishClubCreate(Long clubId, MultipartFile iconImage, MultipartFile posterImage, String iconImagePath, String posterImagePath) {
         try {
-            ClubCreateEvent.ClubCreateImage poster = null;
-            if (Objects.nonNull(posterImage) && !posterImage.isEmpty()) {
-                poster = new ClubCreateEvent.ClubCreateImage(posterImagePath, posterImage);
+            ClubCreateImage icon = new ClubCreateImage(iconImagePath, iconImage);
+
+            if (posterImage != null) {
+                ClubCreateImage poster = new ClubCreateImage(iconImagePath, posterImage);
+                Events.raise(new ClubCreateEvent(clubId, icon, poster));
+            } else {
+                Events.raise(new ClubCreateEvent(clubId, icon, null));
             }
 
-            Events.raise(new ClubCreateEvent(
-                    clubId,
-                    new ClubCreateEvent.ClubCreateImage(iconImagePath, iconImage),
-                    poster)
-            );
         } catch (IOException e) {
             throw new InvalidStateException(ErrorCode.FILE_IO_EXCEPTION);
         }
