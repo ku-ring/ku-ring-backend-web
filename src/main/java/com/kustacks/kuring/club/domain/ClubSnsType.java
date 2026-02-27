9d517ad345525283e327e1776c16590b2de046dc
Han Ju Kim <hjkim2902@gmail.com>
2026-02-27 17:32:29 +0900

    fix : ClubSnsType 매핑 과정 구체화 및 예외 케이스 처리

--- src/main/java/com/kustacks/kuring/club/domain/ClubSnsType.java
+++ src/main/java/com/kustacks/kuring/club/domain/ClubSnsType.java
@@ -1,25 +1,48 @@
 package com.kustacks.kuring.club.domain;
 
-import java.util.Arrays;
-import java.util.List;
+import com.kustacks.kuring.common.exception.InvalidStateException;
+import com.kustacks.kuring.common.exception.code.ErrorCode;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public enum ClubSnsType {
-    INSTAGRAM(List.of("https://www.instagram.com", "https://instagram.com")),
-    YOUTUBE(List.of("https://www.youtube.com", "https://youtube.com")),
-    ETC(List.of());
+    INSTAGRAM(Set.of("instagram.com", "www.instagram.com", "m.instagram.com")),
+    YOUTUBE(Set.of("youtube.com", "www.youtube.com", "m.youtube.com", "youtu.be")),
+    ETC(Set.of());
 
-    private List<String> urls;
+    private final Set<String> hosts;
 
-    ClubSnsType(List<String> urls) {
-        this.urls = urls;
+    private static final Map<String, ClubSnsType> HOST_TO_TYPE;
+
+    static {
+        Map<String, ClubSnsType> map = new HashMap<>();
+        for (ClubSnsType type : ClubSnsType.values()) {
+            for (String host : type.hosts) {
+                map.put(host, type);
+            }
+        }
+        HOST_TO_TYPE = Collections.unmodifiableMap(map);
+    }
+
+    ClubSnsType(Set<String> hosts) {
+        this.hosts = hosts;
     }
 
     public static ClubSnsType fromUrl(String url) {
-        return Arrays.stream(ClubSnsType.values())
-                .filter(
-                        clubSnsType -> clubSnsType.urls.stream()
-                                .anyMatch(url::startsWith)
-                ).findFirst()
-                .orElse(ETC);
+        String host = extractHost(url);
+        return HOST_TO_TYPE.getOrDefault(host, ETC);
+    }
+
+    private static String extractHost(String url) {
+        try {
+            URI uri = URI.create(url.trim());
+            return uri.getHost();
+        } catch (IllegalArgumentException e) {
+            throw new InvalidStateException(ErrorCode.API_INVALID_PARAM);
+        }
     }
 }
