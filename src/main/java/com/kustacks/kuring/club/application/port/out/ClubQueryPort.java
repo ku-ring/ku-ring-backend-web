dcf603a1dd9316ff8d1bc1a1e0ce5c8bddb67020
Han Ju Kim <hjkim2902@gmail.com>
2026-02-27 16:17:19 +0900

    feat : 동아리 중복 조회 포트 메서드 추가

--- src/main/java/com/kustacks/kuring/club/application/port/out/ClubQueryPort.java
+++ src/main/java/com/kustacks/kuring/club/application/port/out/ClubQueryPort.java
@@ -1,6 +1,7 @@
 package com.kustacks.kuring.club.application.port.out;
 
 import com.kustacks.kuring.club.domain.Club;
+import com.kustacks.kuring.club.domain.ClubDivision;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -13,4 +14,6 @@ public interface ClubQueryPort {
     List<Club> findClubsBetweenDates(LocalDateTime start, LocalDateTime end);
 
     List<Club> findNextDayRecruitEndClubs(LocalDateTime now);
+
+    boolean existsByNameAndDivision(String name, ClubDivision division);
 }
