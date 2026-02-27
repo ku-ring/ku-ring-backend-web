b88900eaf1719f9cfb7a2340b623d831a7fe7f49
Han Ju Kim <hjkim2902@gmail.com>
2026-02-27 16:17:19 +0900

    feat : SNS URL 기반 타입 판별 로직 추가

--- src/main/java/com/kustacks/kuring/club/domain/ClubSnsType.java
+++ src/main/java/com/kustacks/kuring/club/domain/ClubSnsType.java
@@ -1,7 +1,25 @@
 package com.kustacks.kuring.club.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public enum ClubSnsType {
-    INSTAGRAM,
-    YOUTUBE,
-    ETC
+    INSTAGRAM(List.of("https://www.instagram.com", "https://instagram.com")),
+    YOUTUBE(List.of("https://www.youtube.com", "https://youtube.com")),
+    ETC(List.of());
+
+    private List<String> urls;
+
+    ClubSnsType(List<String> urls) {
+        this.urls = urls;
+    }
+
+    public static ClubSnsType fromUrl(String url) {
+        return Arrays.stream(ClubSnsType.values())
+                .filter(
+                        clubSnsType -> clubSnsType.urls.stream()
+                                .anyMatch(url::startsWith)
+                ).findFirst()
+                .orElse(ETC);
+    }
 }
