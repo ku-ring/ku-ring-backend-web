78d5250a2edb7559ec7a5503750acfa50d5451a7
jiyun921 <ryn0921@naver.com>
2026-03-03 19:20:28 +0900

    [refactor] SQL 개행 원복

--- src/main/resources/db/migration/V260217__Alter_club_subscribe_to_root_user.sql
+++ src/main/resources/db/migration/V260217__Alter_club_subscribe_to_root_user.sql
@@ -1,18 +1,16 @@
 -- root_user로 바꾸면 데이터 이관을 해야하나, 아직 데이터가 존재하지 않는 개발 단계이므로 데이터 삭제하고 진행.
 -- 1) 기존 device(user) 기준 구독 데이터는 유지하지 않는다.
-DELETE
-FROM club_subscribe;
+DELETE FROM club_subscribe;
 
 -- 2) 기존 user FK/unique/index 제약 제거
 ALTER TABLE club_subscribe
-DROP
-FOREIGN KEY fk_club_subscribe_user;
+    DROP FOREIGN KEY fk_club_subscribe_user;
 
 ALTER TABLE club_subscribe
-DROP INDEX uk_club_user;
+    DROP INDEX uk_club_user;
 
 ALTER TABLE club_subscribe
-DROP INDEX idx_club_subscribe_user;
+    DROP INDEX idx_club_subscribe_user;
 
 -- 3) root_user_id 컬럼 추가
 ALTER TABLE club_subscribe
@@ -20,8 +18,7 @@ ALTER TABLE club_subscribe
 
 -- 4) 기존 user_id 컬럼 제거
 ALTER TABLE club_subscribe
-DROP
-COLUMN user_id;
+    DROP COLUMN user_id;
 
 -- 5) root_user FK 추가 (동아리/계정 삭제 시 구독도 함께 삭제)
 ALTER TABLE club_subscribe
