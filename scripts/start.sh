736bf203fffc8347f1e5cba525c8b5e286f89f91
HanJu Kim <56250226+rlagkswn00@users.noreply.github.com>
2026-03-30 22:11:06 +0900

    fix: CodeDeploy stdout redirect (#289) (#366)

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,6 +3,7 @@
 PROJECT_ROOT="/home/ec2-user/app"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+APP_LOG="$PROJECT_ROOT/application.log"
 
 TIME_NOW=$(date +%c)
 
@@ -18,7 +19,7 @@ cd $PROJECT_ROOT
 # jar 파일 실행
 chmod 755 $JAR_FILE
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Dspring.profiles.active=prod -Xms256m -Xmx256m $JAR_FILE &
+nohup java -jar -Dspring.profiles.active=prod -Xms256m -Xmx256m $JAR_FILE > "$APP_LOG" 2>&1 < /dev/null &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
