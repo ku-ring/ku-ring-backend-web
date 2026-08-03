5a82ac7f4715ccedf1c8914ec788e32b515669f8
juneon <juneon0830@naver.com>
2026-08-04 00:07:25 +0900

    Refactor: 캠퍼스맵 건물 키워드 검색 쿼리 개선

--- src/main/java/com/kustacks/kuring/building/adapter/out/persistence/BuildingQueryRepositoryImpl.java
+++ src/main/java/com/kustacks/kuring/building/adapter/out/persistence/BuildingQueryRepositoryImpl.java
@@ -1,19 +1,15 @@
 package com.kustacks.kuring.building.adapter.out.persistence;
 
 import com.kustacks.kuring.building.domain.Building;
-import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 import static com.kustacks.kuring.building.domain.QBuilding.building;
-import static com.kustacks.kuring.building.domain.QBuildingSearchKeyword.buildingSearchKeyword;
 
 @RequiredArgsConstructor
 class BuildingQueryRepositoryImpl implements BuildingQueryRepository {
-
-    private static final char LIKE_ESCAPE = '\\';
 
     private final JPAQueryFactory queryFactory;
 
@@ -21,25 +17,12 @@ class BuildingQueryRepositoryImpl implements BuildingQueryRepository {
     public List<Building> searchByKeyword(String keyword) {
         return queryFactory
                 .selectFrom(building)
-                .leftJoin(building.keywords, buildingSearchKeyword)
-                .where(keywordContains(keyword))
-                .distinct()
+                .where(
+                        building.name.containsIgnoreCase(keyword)
+                                .or(building.address.containsIgnoreCase(keyword))
+                                .or(building.keywords.any().keyword.containsIgnoreCase(keyword))
+                )
                 .orderBy(building.id.asc())
                 .fetch();
-    }
-
-    private BooleanExpression keywordContains(String keyword) {
-        String pattern = "%" + escapeLikePattern(keyword) + "%";
-
-        return building.name.likeIgnoreCase(pattern, LIKE_ESCAPE)
-                .or(building.address.likeIgnoreCase(pattern, LIKE_ESCAPE))
-                .or(buildingSearchKeyword.keyword.likeIgnoreCase(pattern, LIKE_ESCAPE));
-    }
-
-    private String escapeLikePattern(String keyword) {
-        return keyword
-                .replace("\\", "\\\\")
-                .replace("%", "\\%")
-                .replace("_", "\\_");
     }
 }
